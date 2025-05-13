7500255f0280e708d42941e450a0470ba228cda9
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-13 19:17:25 +0300

    add project completion modal component

---
+++ app/components/modals/ProjectCompletionModal/ProjectCompletionFormAction.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { db } from '@db'; // Database instance
+import { getUserIndex } from '@db/actions';
+import { getUserId } from '@server';
+import { ProjectCompletionSchema } from '@zodSchemas';
+
+export const projectCompletionFormAction = async (formData: FormData) => {
+  try {
+    const formEntries = formData.entries();
+    const rawData = Object.fromEntries(formEntries);
+
+    const responseData = { ...rawData };
+
+    const {
+      success,
+      data: validatedData,
+      error,
+    } = ProjectCompletionSchema.safeParse(responseData);
+
+    console.log(success, validatedData, error);
+
+    await db.read();
+
+    const userId = await getUserId();
+    if (!userId) {
+      throw new Error(
+        'Invalid User Id (app/components/modals/ProjectCompletionModal/ProjectCompletionFormActions.ts)'
+      );
+    }
+    if (!success) {
+      throw new Error(
+        'Validation Error (app/components/modals/ProjectCompletionModal/ProjectCompletionFormActions.ts)',
+        error
+      );
+    }
+    const userIndex = await getUserIndex(userId);
+    db.data.users[userIndex].projectCompletion.push(validatedData);
+
+    await db.write();
+  } catch (error) {
+    console.error(error);
+  }
+};
