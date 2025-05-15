776180dd8c111e045d84a7280a085a39cfe26dc9
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-15 17:35:16 +0300

    add types ( purchase deatils, batch product, purchase product )

---
+++ app/types/pages/request/PurchaseProduct.ts
@@ -0,0 +1,15 @@
+export type PurchaseProduct = {
+  id: string;
+  product: string;
+  description: string;
+  quantity: string;
+  completedQuantity: string;
+  completedCost: string;
+  underCompletedQauntity: string;
+  remainingQuantity: string;
+  remainingCost: string;
+  unitPrice: string;
+  unitPriceWithTax: string;
+  subtotal: string;
+  subtotalWithTax: string;
+};
