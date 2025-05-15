1a5ed890cfe0ba2a0e909fe05d6cef403cd65ed0
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-15 17:32:27 +0300

    add batch product modal

---
+++ app/components/modals/BatchProductsModal/BatchProductsFormAction.ts
@@ -0,0 +1,44 @@
+'use server';
+
+import { db } from '@db'; // Database instance
+import { getUserIndex } from '@db/actions';
+import { getUserId } from '@server';
+import { BatchProductSchema } from '@zodSchemas';
+
+export const batchProductsFormAction = async (formData: FormData) => {
+  try {
+    const formEntries = formData.entries();
+    const rawData = Object.fromEntries(formEntries);
+
+    const responseData = { ...rawData };
+
+    const {
+      success,
+      data: validatedData,
+      error,
+    } = BatchProductSchema.safeParse(responseData);
+
+    await db.read();
+
+    console.log(success, validatedData, error);
+
+    const userId = await getUserId();
+    if (!userId) {
+      throw new Error(
+        'Invalid User Id (app/components/modals/BatchProductsModal/BatchProductsFormActions.ts)'
+      );
+    }
+    if (!success) {
+      throw new Error(
+        'Validation Error (app/components/modals/BatchProductsModal/BatchProductsFormActions.ts)',
+        error
+      );
+    }
+    const userIndex = await getUserIndex(userId);
+    db.data.users[userIndex].batchProducts.push(validatedData);
+
+    await db.write();
+  } catch (error) {
+    console.error(error);
+  }
+};
