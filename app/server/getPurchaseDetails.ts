81552008bda8f6f351169d794437b9d417788693
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-15 17:34:13 +0300

    add get purchase details endpoint

---
+++ app/server/getPurchaseDetails.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { PurchaseDetails } from '@types';
+import { getPurchaseProductsByRequestId } from './getPurchaseProductsByRequestId';
+
+export const getPurchaseDetails = async (
+  id: string
+): Promise<PurchaseDetails | void> => {
+  const purchaseProducts = await getPurchaseProductsByRequestId({
+    purchaseRequestId: id,
+  });
+  return { ...dummyData, purchaseProducts };
+};
+
+const dummyData: PurchaseDetails = {
+  id: '1',
+  requestDate: '17-04-2024',
+  type: 'تشغيلي',
+  requestTitle: 'برامج الابتكار وتبني التقنية',
+  requestOutcomes: 'تحسين الكفاءة التشغيلية وزيادة التفاعل الرقمي.',
+  projectName: 'مبادرة التحول الرقمي',
+  programName: 'برنامج الابتكار الحكومي',
+  planType: 'خطة سنوية',
+  description: 'عملية شراء لتطبيق "سهل+" لتحسين تجربة المستخدم.',
+  costs: '250,000 ريال',
+  awardAmount: '245,000 ريال',
+  awardAmountBeforeChange: '250,000 ريال',
+  attachments: [
+    new File([''], 'نموذج طلب 2 .pdf'),
+    new File([''], 'نموذج طلب .pdf'),
+  ],
+  purchaseProducts: [],
+};
