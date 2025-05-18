975d5a2469355a2d6c18aa78af04eb2367984bc7
yasser <yasser@gamil.com>
2025-05-18 11:27:45 +0300

    work in progress

---
+++ app/server/getFamilyNewsList.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import type { NewsFamily } from "@types";
+import { getDemo } from "../db/actions/getDemo";
+import { getFetchHeaders } from "./getFetchHeaders";
+import { NewsElementSchema, ResponseSchema } from "../../schemas";
+
+export const getFamilyNewsList = async (): Promise<NewsFamily[]> => {
+  const isDemo = await getDemo();
+  if (isDemo) return dummyData;
+
+  // ! VARIBLES
+  // ! ==================================
+  const url = "api/po/read/portal-news";
+  const apiRootUrl = process.env.API_ROOT_URL as string;
+  const { headers } = await getFetchHeaders();
+  const requestBody = { "news_type":"family_news" };
+  const requestBodyString = JSON.stringify(requestBody);
+  const requestUrl = `${apiRootUrl}/${url}`;
+
+  // ! FETCH
+  // ! ==================================
+  const apiResponse = await fetch(requestUrl, {
+    headers,
+    method: "POST",
+    body: requestBodyString,
+  });
+  const responseJson = await apiResponse.json();
+
+  // ! VALIDATION
+  // ! ==================================
+  const validatedResponse = ResponseSchema.parse(responseJson);
+  const { result } = validatedResponse;
+  const { data } = result;
+  const validatedData = NewsElementSchema.array().parse(data);
+
+  // ! PARSING
+  // ! ==================================
+  const returnedData: NewsFamily[] = validatedData.map((data) => {
+    const newsItem: NewsFamily = {
+      id: data.id,
+      title: data.title,
+      date: new Date(data.create_date).toLocaleDateString("ar-EG", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+      image: data.image
+        ? `data:image/gif;base64,${data.image}`
+        : "/monshaatFamily-1.svg",
+    };
+    return newsItem;
+  });
+  return returnedData;
+};
+
+const dummyData: NewsFamily[] = [
+  {
+    id: 1,
+    title:
+      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
+    date: "01.06.2024 - 07:54 صباحاً",
+    image: "/monshaatFamily-1.svg",
+  },
+  {
+    id: 2,
+    title:
+      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
+    date: "01.06.2024 - 07:54 صباحاً",
+    image: "/monshaatFamily-2.svg",
+  },
+  {
+    id: 3,
+    title:
+      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
+    date: "01.06.2024 - 07:54 صباحاً",
+    image: "/monshaatFamily-3.svg",
+  },
+  {
+    id: 4,
+    title:
+      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
+    date: "01.06.2024 07:54 صباحًا",
+    image: "/MonshaatFamily-4.svg",
+  },
+];
