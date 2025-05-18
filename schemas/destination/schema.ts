b006f6d8dcfc9f123e8d0296aca2fcf5b90519fb
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-18 12:36:36 +0300

    delete schemas folder

--- schemas/destination/schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-
-export const DestinationElementSchema = z.object({
-    "id": z.number(),
-    "name": z.string(),
-});
-export type DestinationElement = z.infer<typeof DestinationElementSchema>;
