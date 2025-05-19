622b0a2ca1a5b364fa7ce499a3acbf2afe917dd4
Tahani Samir <t.samirmohamed@gmail.com>
2025-05-19 10:00:25 +0300

    add comment

--- app/(pages)/request/supplier-evaluation/components/EvaluationForm/index.tsx
+++ app/(pages)/request/supplier-evaluation/components/EvaluationForm/index.tsx
@@ -9,7 +9,7 @@ import {
 } from "../../components";
 
 export const EvaluationForm = async () => {
-  //TODO: this is a temporary solution, we need to get the data from the server
+  //TODO: this is a temporary solution, we need to get the data from the server from a deiffrent endpoint
   const evaluationCriteriaData =
     (await getSupplierEvaluationRequestCriteria("1")) || [];
 
