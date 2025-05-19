d153fb0f5c05154596115da06294744509438dfb
Tahani Samir <t.samirmohamed@gmail.com>
2025-05-19 14:09:39 +0300

    Add deputations list

--- app/(pages)/request/deputations/page.tsx
+++ app/(pages)/request/deputations/page.tsx
@@ -1,13 +1,13 @@
-import { getWorkDocumentRequests } from '@server';
+import { getDeputationRequests } from '@server';
 import { DeputationTable } from './components';
 
-const WorkDocumentListPage = async () => {
-  const workDocumentData = await getWorkDocumentRequests();
+const DeputationListPage = async () => {
+  const DeputationData = await getDeputationRequests();
   return (
     <div className='mb-12'>
-      <DeputationTable data={workDocumentData} />
+      <DeputationTable data={DeputationData} />
     </div>
   );
 };
 
-export default WorkDocumentListPage;
+export default DeputationListPage;
