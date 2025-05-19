d210a6ba6c349e2f783acc548ec5f928b905bfb6
Tahani Samir <t.samirmohamed@gmail.com>
2025-05-19 11:35:36 +0300

    add module

---
+++ app/(pages)/request/deputations/new/page.tsx
@@ -0,0 +1,18 @@
+import { RequestStatus } from '@components';
+import { getRequestStatus } from '@server';
+import { WorkDocumentForm } from '../components';
+
+
+const NewWorkDocumentPage = async () => {
+  const requestStatus = await getRequestStatus();
+  const requestCaption =
+    'انت الان في مرحلة انشاء الطلب و بانتظار موافقة المدير المباشر';
+  return (
+    <div className='space-y-4 mb-16'>
+      <RequestStatus status={requestStatus} caption={requestCaption} />
+      <WorkDocumentForm />
+    </div>
+  );
+};
+
+export default NewWorkDocumentPage;
