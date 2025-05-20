3ad2e9b7344457df14b781397582db7d7c44c1a3
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-20 13:37:51 +0300

    add training list page and training layout

---
+++ app/(pages)/request/training/page.tsx
@@ -0,0 +1,18 @@
+import { Instructions } from '@components';
+import { getTrainingRequests } from '@server';
+import { TrainingTable } from './components';
+
+const TrainingListPage = async () => {
+  const trainingData = await getTrainingRequests();
+  return (
+    <div className='space-y-4 mb-12'>
+      <TrainingTable data={trainingData} />
+      <Instructions
+        title='توضيحات حول الخدمة'
+        description='تتيح هذه الخدمة للموظف إمكانية عرض وتقديم طلبات الدورات التدريبية.'
+      />
+    </div>
+  );
+};
+
+export default TrainingListPage;
