421b486e8e5ce96ee745e970b81e45bdd9879bb5
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-21 00:32:18 +0300

    add config file

---
+++ app/(pages)/request/training/components/config.ts
@@ -0,0 +1,19 @@
+export const TrainingTypes = [
+  { id: '1', name: 'برنامج تدريبي' },
+  { id: '2', name: 'شهادة احترافية' },
+  { id: '3', name: 'معرض او مؤتمر' },
+  { id: '4', name: 'رخصة تعلم إلكتروني' },
+];
+
+export const TrainingCenters = [
+  { id: '1', name: 'الكلية التقنية' },
+  { id: '2', name: 'مركز فكر الأعمال للتدريب' },
+  { id: '3', name: 'معهد الإدارة العامة' },
+  { id: '4', name: 'كلية ينبع الصناعية' },
+  { id: '5', name: 'كلية المدربين التقنيين' },
+];
+
+export const SubstituteEmployees = [
+  { id: 1, name: 'عاصم بن رشود العصيمي' },
+  { id: 2, name: 'محمد بن علي الرفاعي' },
+];
