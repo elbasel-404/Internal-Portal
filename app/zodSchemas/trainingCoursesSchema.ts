9c2720428d4983fc291937db1a56fe798fb20652
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-21 18:03:36 +0300

    add training course schema

---
+++ app/zodSchemas/trainingCoursesSchema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const TrainingCoursesSchema = z.object({
+  dateFrom: z.string().min(1),
+  dateTo: z.string().min(1),
+  travelDateSettings: z.string().optional(),
+  duration: z.string().optional(),
+});
