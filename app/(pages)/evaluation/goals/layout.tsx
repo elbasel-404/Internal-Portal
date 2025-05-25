83df47245a5396dd319da1bdb0ceb03db244ec99
Tahani Samir <t.samirmohamed@gmail.com>
2025-05-25 15:41:28 +0300

    Add Evaluation Goals List

---
+++ app/(pages)/evaluation/goals/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { EvaluationGoalsData } from './components';
+
+export const metadata = {
+  title: 'Evaluation Goals',
+  description: 'Evaluation Goals Data',
+};
+interface EvaluationGoalsLayoutProps {
+  children: ReactNode;
+}
+const EvaluationGoalsLayout = ({ children }: EvaluationGoalsLayoutProps) => {
+  return (
+    <>
+      <EvaluationGoalsData />
+      <section>{children}</section>
+    </>
+  );
+};
+
+export default EvaluationGoalsLayout;
