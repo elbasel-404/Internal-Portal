f0e547879e36d3e3e00ba3eab43848032ae17c64
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-26 12:59:20 +0300

    update formatDate to formatNewsDate

--- app/server/getFamilyNewsList.ts
+++ app/server/getFamilyNewsList.ts
@@ -1,10 +1,10 @@
-"use server";
+'use server';
 
-import type { NewsFamily } from "@types";
-import { getDemo } from "../db/actions/getDemo";
-import { getFetchHeaders } from "./getFetchHeaders";
-import { FamilyNewSchema, ResponseSchema } from "@api/schemas";
-import { formatDate } from "@utils";
+import { FamilyNewSchema, ResponseSchema } from '@api/schemas';
+import type { NewsFamily } from '@types';
+import { formatNewsDate } from '@utils';
+import { getDemo } from '../db/actions/getDemo';
+import { getFetchHeaders } from './getFetchHeaders';
 
 export const getFamilyNewsList = async (): Promise<NewsFamily[]> => {
   const isDemo = await getDemo();
@@ -12,10 +12,10 @@ export const getFamilyNewsList = async (): Promise<NewsFamily[]> => {
 
   // ! VARIBLES
   // ! ==================================
-  const url = "api/po/read/portal-news";
+  const url = 'api/po/read/portal-news';
   const apiRootUrl = process.env.API_ROOT_URL as string;
   const { headers } = await getFetchHeaders();
-  const requestBody = { news_type: "family_news" };
+  const requestBody = { news_type: 'family_news' };
   const requestBodyString = JSON.stringify(requestBody);
   const requestUrl = `${apiRootUrl}/${url}`;
 
@@ -23,7 +23,7 @@ export const getFamilyNewsList = async (): Promise<NewsFamily[]> => {
   // ! ==================================
   const apiResponse = await fetch(requestUrl, {
     headers,
-    method: "POST",
+    method: 'POST',
     body: requestBodyString,
   });
   const responseJson = await apiResponse.json();
@@ -41,10 +41,10 @@ export const getFamilyNewsList = async (): Promise<NewsFamily[]> => {
     const newsItem: NewsFamily = {
       id: data.id,
       title: data.title,
-      date: formatDate(data.create_date),
+      date: formatNewsDate(data.create_date),
       image: data.image
         ? `data:image/gif;base64,${data.image}`
-        : "/monshaatFamily-1.svg",
+        : '/monshaatFamily-1.svg',
       description: data.resume,
     };
     return newsItem;
@@ -56,33 +56,33 @@ const dummyData: NewsFamily[] = [
   {
     id: 1,
     title:
-      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
-    date: "01.06.2024 - 07:54 صباحاً",
-    image: "/monshaatFamily-1.svg",
-    description: "",
+      'رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...',
+    date: '01.06.2024 - 07:54 صباحاً',
+    image: '/monshaatFamily-1.svg',
+    description: '',
   },
   {
     id: 2,
     title:
-      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
-    date: "01.06.2024 - 07:54 صباحاً",
-    image: "/monshaatFamily-2.svg",
-    description: "",
+      'رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...',
+    date: '01.06.2024 - 07:54 صباحاً',
+    image: '/monshaatFamily-2.svg',
+    description: '',
   },
   {
     id: 3,
     title:
-      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
-    date: "01.06.2024 - 07:54 صباحاً",
-    image: "/monshaatFamily-3.svg",
-    description: "",
+      'رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...',
+    date: '01.06.2024 - 07:54 صباحاً',
+    image: '/monshaatFamily-3.svg',
+    description: '',
   },
   {
     id: 4,
     title:
-      "رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...",
-    date: "01.06.2024 07:54 صباحًا",
-    image: "/MonshaatFamily-4.svg",
-    description: "",
+      'رزقت الزميلة: لمياء بنت عبدالله الربيعان بأحمد جعله الله من مواليد ال...',
+    date: '01.06.2024 07:54 صباحًا',
+    image: '/MonshaatFamily-4.svg',
+    description: '',
   },
 ];
