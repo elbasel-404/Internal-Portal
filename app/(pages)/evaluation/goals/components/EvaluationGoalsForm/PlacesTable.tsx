2824c5c77ad6d40a5abb8d96a49e1161883806e0
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-26 13:33:14 +0300

    fix build issue

--- app/(pages)/evaluation/goals/components/EvaluationGoalsForm/PlacesTable.tsx
+++ app/(pages)/evaluation/goals/components/EvaluationGoalsForm/PlacesTable.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { DeputationPlace  } from "@types";
+import { CheckboxField } from '@components/form';
+import { ModalLink } from '@components/modals/ModalLink';
+import { CirclePlusIcon, TrashIcon } from '@icons';
+import { DeputationPlace } from '@types';
 import {
+  Button,
   TableBody,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
   Table as UITable,
-  Button,
-} from "@ui";
-import { CirclePlusIcon, TrashIcon } from "@icons";
-import { CheckboxField } from "@components/form";
-import { ModalLink } from "@components/modals/ModalLink";
+} from '@ui';
 
 interface DeputationPlacesProps {
   //TODO: DeputationPlace Type
@@ -24,9 +24,9 @@ interface DeputationPlacesProps {
 }
 
 const tableHeaders = [
-  { label: "البلاد" },
-  { label: "المدينة" },
-  { label: "الاجراءات" },
+  { label: 'البلاد' },
+  { label: 'المدينة' },
+  { label: 'الاجراءات' },
 ];
 
 export const PlacesTable = ({
@@ -36,35 +36,35 @@ export const PlacesTable = ({
   onRemove,
 }: DeputationPlacesProps) => {
   function changeIssueVisa(value: boolean): void {
-    throw new Error("Function not implemented.");
+    throw new Error('Function not implemented.');
   }
 
   return (
     <>
-      <div className="bg-white rounded-lg m-6">
-        <div className="flex border-r-4 items-center bg-[#007C9E24] border-[#007497] py-3 px-2 mb-2">
-          <div className="flex gap-2 slotHandle p-4 flex-1">
-            <h2 className="text-2xl font-bold">
-              مكان الانتداب <span className="text-red-500">*</span>
+      <div className='bg-white rounded-lg m-6'>
+        <div className='flex border-r-4 items-center bg-[#007C9E24] border-[#007497] py-3 px-2 mb-2'>
+          <div className='flex gap-2 slotHandle p-4 flex-1'>
+            <h2 className='text-2xl font-bold'>
+              مكان الانتداب <span className='text-red-500'>*</span>
             </h2>
           </div>
           <div>
             <ModalLink
-              name="DeputationPlacesModal"
-              className="flex group text-sm font-medium items-center gap-2 bg-primary text-white px-4 py-1 rounded-full hover:bg-primary-opacity hover:text-primary border-2 border-primary"
+              name='DeputationPlacesModal'
+              className='flex group text-sm font-medium items-center gap-2 bg-primary text-white px-4 py-1 rounded-full hover:bg-primary-opacity hover:text-primary border-2 border-primary'
             >
-              <CirclePlusIcon className="fill-white group-hover:fill-primary" />
+              <CirclePlusIcon className='fill-white group-hover:fill-primary' />
               اضافة مكان الانتداب
             </ModalLink>
           </div>
         </div>
         <UITable>
-          <TableHeader className="bg-cloudGray">
+          <TableHeader className='bg-cloudGray'>
             <TableRow>
               {tableHeaders.map((col, index) => (
                 <TableHead
                   key={index}
-                  className={"text-right text-darkBlue text-lg w-1/12"}
+                  className={'text-right text-darkBlue text-lg w-1/12'}
                 >
                   {col.label}
                 </TableHead>
@@ -75,24 +75,24 @@ export const PlacesTable = ({
             {data.map((item, index) => (
               <TableRow
                 key={item.id}
-                className={`${index % 2 !== 0 ? "bg-cloudGray" : "bg-white"}`}
+                className={`${index % 2 !== 0 ? 'bg-cloudGray' : 'bg-white'}`}
               >
                 {Object.entries(item)
-                  .filter(([key]) => key !== "id")
+                  .filter(([key]) => key !== 'id')
                   .map(([key, value]) => (
-                    <TableCell key={key} className="w-1/12">
+                    <TableCell key={key} className='w-1/12'>
                       {value}
                     </TableCell>
                   ))}
-                <TableCell className="w-1/12">
+                <TableCell className='w-1/12'>
                   <Button
-                    className="flex group gap-1 items-center shadow-none hover:bg-red-600 hover:text-white justify-end text-destructive-foreground bg-destructive-opacity rounded-xl px-4 py-2.5"
+                    className='flex group gap-1 items-center shadow-none hover:bg-red-600 hover:text-white justify-end text-destructive-foreground bg-destructive-opacity rounded-xl px-4 py-2.5'
                     onClick={(e) => {
                       e.preventDefault();
                       onRemove && onRemove(item.id);
                     }}
                   >
-                    <TrashIcon className="fill-destructive-foreground group-hover:fill-white" />
+                    <TrashIcon className='fill-destructive-foreground group-hover:fill-white' />
                     حذف
                   </Button>
                 </TableCell>
@@ -100,13 +100,13 @@ export const PlacesTable = ({
             ))}
           </TableBody>
         </UITable>
-        <div className="my-10">
+        <div className='my-10'>
           <CheckboxField
-            name="issueVisa"
-            label="ارغب في اصدار تأشيرة سفر من قبل منشآت  "
-            className="flex md:items-center gap-x-3"
-            labelStyle="text-lg text-black font-medium leading-0"
-            checkboxStyle="mt-1 md:mt-0"
+            name='issueVisa'
+            label='ارغب في اصدار تأشيرة سفر من قبل منشآت  '
+            className='flex md:items-center gap-x-3'
+            labelStyle='text-lg text-black font-medium leading-0'
+            checkboxStyle='mt-1 md:mt-0'
             checked={issueVisa}
             onChange={(value) => onChangeIssueVisa(value)}
           />
