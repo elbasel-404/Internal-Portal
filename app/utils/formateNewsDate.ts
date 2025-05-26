4618e81a93edb935b0eb237d72cff83947c27492
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-26 12:58:33 +0300

    create format news date function

---
+++ app/utils/formateNewsDate.ts
@@ -0,0 +1,12 @@
+export const formatDate = (date: Date) => {
+  const dateToFormat = new Date(date);
+  const day = dateToFormat.toLocaleString("en-US", { day: "2-digit" });
+  const month = dateToFormat.toLocaleString("en-US", { month: "2-digit" });
+  const year = dateToFormat.toLocaleString("en-US", { year: "numeric" });
+  let hours = dateToFormat.getHours();
+  const minutes = dateToFormat.getMinutes().toString().padStart(2, "0");
+  const period = hours < 12 ? "صباحًا" : "مساءً";
+  hours = hours % 12 || 12;
+  const hourString = hours.toString().padStart(2, "0");
+  return `${day}.${month}.${year} - ${hourString}:${minutes} ${period}`;
+};
