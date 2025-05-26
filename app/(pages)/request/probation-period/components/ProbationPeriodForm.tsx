78d44c898c42ed183381dac83f66f44af412c539
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-26 12:56:39 +0300

    add form actions to probation evaluation form

--- app/(pages)/request/probation-period/components/ProbationPeriodForm.tsx
+++ app/(pages)/request/probation-period/components/ProbationPeriodForm.tsx
@@ -12,7 +12,12 @@ import {
 } from '@components/form';
 import { paths } from '@lib';
 import { ProbationPeriodEmployees, ProbationPeriodFields } from '@types';
-import { useState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import { formAction } from './helpers/formAction';
+import { getStateAction } from './helpers/getStateAction';
+import { initialState } from './helpers/initialState';
+import { State } from './helpers/State';
 
 interface ProbationPeriodFormProps {
   probationPeriodEmployees: ProbationPeriodEmployees[];
@@ -21,12 +26,15 @@ interface ProbationPeriodFormProps {
   probationPeriodRecommendation: ProbationPeriodFields[];
 }
 
+const stateAction = getStateAction<State>(formAction);
+
 export const ProbationPeriodForm = ({
   probationPeriodEmployees,
   probationPeriodQuestions,
   probationPeriodAnswers,
   probationPeriodRecommendation,
 }: ProbationPeriodFormProps) => {
+  const [state, action, pending] = useActionState(stateAction, initialState);
   const [files, setFiles] = useState<File[]>([]);
   const [employeeName, setEmployeeName] = useState('');
   const [jobNumber, setJobNumber] = useState('');
@@ -72,8 +80,33 @@ export const ProbationPeriodForm = ({
     }
   };
 
+  useEffect(() => {
+    const { success, errors } = state;
+    if (success) toast.success('تم انشاء الطلب بنجاح');
+    if (errors) toast.error(errors);
+  }, [state]);
+
+  useEffect(() => {
+    if (pending) {
+      toast.loading('جاري انشاء الطلب', {
+        id: 'vacation-form-loading-toast',
+      });
+    } else {
+      toast.dismiss('vacation-form-loading-toast');
+    }
+  }, [pending]);
+
+  if (state.success) {
+    return (
+      <div className='bg-white text-black text-lg p-4 space-y-4'>
+        <p className='text-center'>تم انشاء الطلب بنجاح</p>
+        <p className='text-center'>رقم الطلب: {state.id}</p>
+      </div>
+    );
+  }
+
   return (
-    <form className='bg-white rounded-md p-4'>
+    <form action={action} className='bg-white rounded-md p-4'>
       <FormHeader
         label='نموذج طلب تقييم فترة التجربة'
         path={paths.probationPeriod.href}
@@ -81,7 +114,7 @@ export const ProbationPeriodForm = ({
       <div className='p-4 space-y-6'>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           <SelectField
-            name='employeeName'
+            name='employee_id'
             label='اسم الموظف'
             placeholder='...'
             types={probationPeriodEmployees.map(({ id, employeeName }) => ({
@@ -147,76 +180,91 @@ export const ProbationPeriodForm = ({
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
           {probationPeriodQuestions?.slice(0, 4).map((question, index) => (
-            <SelectField
-              label={question.name}
-              name={question.name}
-              placeholder=''
-              types={probationPeriodAnswers ?? []}
-              value={
-                {
-                  0: organizationCulture,
-                  1: workOutputQuality,
-                  2: responsibility,
-                  3: initiative,
-                }[index] || ''
-              }
-              onChange={(value) => {
-                switch (index) {
-                  case 0:
-                    setOrganizationCulture(value);
-                    break;
-                  case 1:
-                    setWorkOutputQuality(value);
-                    break;
-                  case 2:
-                    setResponsibility(value);
-                    break;
-                  case 3:
-                    setInitiative(value);
-                    break;
+            <div key={question.id} className='space-y-2'>
+              <input
+                type='hidden'
+                name={'question_id' + question.id}
+                value={question.id}
+              />
+              <SelectField
+                label={question.name}
+                name={'answer_id' + question.id}
+                placeholder=''
+                types={probationPeriodAnswers ?? []}
+                value={
+                  {
+                    0: organizationCulture,
+                    1: workOutputQuality,
+                    2: responsibility,
+                    3: initiative,
+                  }[index] || ''
                 }
-              }}
-            />
+                onChange={(value) => {
+                  switch (index) {
+                    case 0:
+                      setOrganizationCulture(value);
+                      break;
+                    case 1:
+                      setWorkOutputQuality(value);
+                      break;
+                    case 2:
+                      setResponsibility(value);
+                      break;
+                    case 3:
+                      setInitiative(value);
+                      break;
+                  }
+                }}
+              />
+            </div>
           ))}
         </div>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           {probationPeriodQuestions?.slice(4, 7).map((question, index) => (
-            <SelectField
-              label={question.name}
-              name={question.name}
-              placeholder=''
-              types={probationPeriodAnswers ?? []}
-              value={
-                {
-                  0: policyCompliance,
-                  1: teamWork,
-                  2: communicationSkills,
-                }[index] || ''
-              }
-              onChange={(value) => {
-                switch (index) {
-                  case 0:
-                    setPolicyCompliance(value);
-                    break;
-                  case 1:
-                    setTeamWork(value);
-                    break;
-                  case 2:
-                    setCommunicationSkills(value);
-                    break;
+            <div key={question.id} className='space-y-2'>
+              <input
+                type='hidden'
+                name={'question_id' + question.id}
+                value={question.id}
+              />
+              <SelectField
+                label={question.name}
+                name={'answer_id' + question.id}
+                placeholder=''
+                types={probationPeriodAnswers ?? []}
+                value={
+                  {
+                    0: policyCompliance,
+                    1: teamWork,
+                    2: communicationSkills,
+                  }[index] || ''
                 }
-              }}
-            />
+                onChange={(value) => {
+                  switch (index) {
+                    case 0:
+                      setPolicyCompliance(value);
+                      break;
+                    case 1:
+                      setTeamWork(value);
+                      break;
+                    case 2:
+                      setCommunicationSkills(value);
+                      break;
+                  }
+                }}
+              />
+            </div>
           ))}
         </div>
 
         <TextareaField
-          name='permissonReason'
+          name='notes'
           label='ملاحظات'
           placeholder='ملاحظات حول الطلب'
           required={false}
         />
         <AttachmentsField
+          name='attachment_ids'
           files={files}
           handleFileUpload={handleFileUpload}
           handleRemoveFile={handleRemoveFile}
