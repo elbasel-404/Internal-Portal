e56b971952fe5ab1a8cd00a3897b8fe1cff7c60a
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-27 13:07:27 +0300

    adjust app for dynamic employee id - test 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { InitUser, Login } from "@components";
+import { InitUser, Login, RegisterChartJSPlugins } from "@components";
 import { getSession } from "@server";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./(pages)/globals.css";
-
 export const dynamic = "force-dynamic";
 
 export const metadata: Metadata = {
@@ -21,6 +20,7 @@ const RootLayout = async ({ children }: Readonly<RootLayoutProps>) => {
       <html>
         <body>
           <InitUser />
+          <RegisterChartJSPlugins />
           <Login />
         </body>
       </html>
