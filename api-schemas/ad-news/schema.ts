a88c7c2fd69b4fe864bd384f42fdb9abad3c7599
Yasser <yassermohamed654321@gmail.com>
2025-05-28 12:28:09 +0300

    fix schema files

--- api-schemas/ad-news/schema.ts
+++ api-schemas/ad-news/schema.ts
@@ -1,26 +1,6 @@
 import * as z from "zod";
 
 
-export const CreateUidEnumSchema = z.enum([
-    "Abdulmajeed A. Alrasheed",
-    "Majid A Albaddah",
-    "Monirah F. Alrabiah",
-    "Reema A. Alsunayyin",
-]);
-export type CreateUidEnum = z.infer<typeof CreateUidEnumSchema>;
-
-
-export const DescriptionSchema = z.enum([
-    "<p><br></p>",
-]);
-export type Description = z.infer<typeof DescriptionSchema>;
-
-
-export const TypeSchema = z.enum([
-    "ads",
-]);
-export type Type = z.infer<typeof TypeSchema>;
-
 export const AdNewSchema = z.object({
     "id": z.number(),
     "published": z.boolean(),
@@ -30,14 +10,14 @@ export const AdNewSchema = z.object({
     "attachment": z.union([z.boolean(), z.string()]),
     "attachment_file_name": z.union([z.boolean(), z.string()]),
     "resume": z.string(),
-    "description": DescriptionSchema,
-    "type": TypeSchema,
+    "description": z.string(),
+    "type": z.string(),
     "tag_ids": z.array(z.any()),
     "comment_ids": z.array(z.number()),
     "like_ids": z.array(z.number()),
-    "create_uid": z.array(z.union([CreateUidEnumSchema, z.number()])),
+    "create_uid": z.array(z.union([z.number(), z.string()])),
     "create_date": z.coerce.date(),
-    "write_uid": z.array(z.union([CreateUidEnumSchema, z.number()])),
+    "write_uid": z.array(z.union([z.number(), z.string()])),
     "write_date": z.coerce.date(),
     "display_name": z.string(),
     "__last_update": z.coerce.date(),
