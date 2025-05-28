b9b3da6ab49b4645d2c3b2792866ff1386142ea8
Yasser <yassermohamed654321@gmail.com>
2025-05-28 12:28:09 +0300

    fix schema files

--- api-schemas/employees-list/schema.ts
+++ api-schemas/employees-list/schema.ts
@@ -3,7 +3,6 @@ import * as z from "zod";
 
 export const EmployeesListElementSchema = z.object({
     "id": z.number(),
-    "replace_employee_id": z.boolean(),
     "department_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
     "parent_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
     "mobile_phone": z.union([z.string(), z.boolean()]),
@@ -14,8 +13,6 @@ export const EmployeesListElementSchema = z.object({
     "job_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
     "complete_name": z.union([z.string(), z.boolean()]),
     "english_name": z.union([z.string(), z.boolean()]),
-    "address_home_id": z.boolean(),
-    "address_id": z.boolean(),
     "emp_state": z.union([z.string(), z.boolean()]),
     "image": z.union([z.string(), z.boolean()]),
     "department_global_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
@@ -24,6 +21,5 @@ export const EmployeesListElementSchema = z.object({
     "administration_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
     "sector_manager_id": z.union([z.array(z.union([z.number(), z.string()])), z.boolean()]),
     "attendance_state": z.union([z.string(), z.boolean()]),
-    "is_governor": z.boolean(),
 });
 export type EmployeesListElement = z.infer<typeof EmployeesListElementSchema>;
