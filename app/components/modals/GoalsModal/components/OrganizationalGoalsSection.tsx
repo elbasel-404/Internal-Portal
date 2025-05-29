6421f128b4c59d15906a415057e0d147e84c5040
alyadelaly <aliadelhacker123@yahoo.com>
2025-05-29 17:12:35 +0300

    add goals modal and its components

---
+++ app/components/modals/GoalsModal/components/OrganizationalGoalsSection.tsx
@@ -0,0 +1,49 @@
+import { InputField, SelectField } from '@components/form';
+
+interface OrganizationalGoalsSectionProps {
+  formData: {
+    organizationalGoalLevel1: string;
+    strategicGoalLevel2: string;
+    strategicGoalLevel3: string;
+  };
+  onInputChange: (name: string, value: string) => void;
+}
+
+export const OrganizationalGoalsSection = ({
+  formData,
+  onInputChange,
+}: OrganizationalGoalsSectionProps) => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+      <InputField
+        label='الهدف المؤسسي المستوى الأول'
+        name='organizationalGoalLevel1'
+        placeholder='تحقيق التميز المؤسسي'
+        value={formData.organizationalGoalLevel1}
+        onChange={(e) =>
+          onInputChange('organizationalGoalLevel1', e.target.value)
+        }
+        required
+        disabled
+      />
+      <InputField
+        label='الهدف الاستراتيجي المستوى الثاني'
+        name='strategicGoalLevel2'
+        placeholder='تحقيق التميز المؤسسي (على مستوى قطاع خدمات منشأت)'
+        value={formData.strategicGoalLevel2}
+        onChange={(e) => onInputChange('strategicGoalLevel2', e.target.value)}
+        required
+        disabled
+      />
+      <SelectField
+        label='الهدف الاستراتيجي المستوى الثالت'
+        name='strategicGoalLevel3'
+        types={[{ id: '1', name: 'تحقيق التميز المؤسسي' }]}
+        placeholder=''
+        value={formData.strategicGoalLevel3}
+        onChange={(value) => onInputChange('strategicGoalLevel3', value)}
+        required
+      />
+    </div>
+  );
+};
