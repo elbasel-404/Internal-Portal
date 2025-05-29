37931b46fff3a0377358afb6889f74e2ea91efe8
Yasser <yassermohamed654321@gmail.com>
2025-05-29 17:49:35 +0300

    passport request

--- app/server/getPassportsRequests.ts
+++ app/server/getPassportsRequests.ts
@@ -1,12 +1,62 @@
 "use server";
 
 import type { PassportRequest } from "@types";
+import { getDemo } from "../db/actions/getDemo";
+import { getFetchHeaders } from "./getFetchHeaders";
+import { PassportRequestSchema, ResponseSchema } from "@api/schemas";
 
 export const getPassportRequests = async (): Promise<PassportRequest[]> => {
-  return PassportDummyData;
+  const isDemo = await getDemo();
+  if (isDemo) return dummyData;
+
+  // ! VARIBLES
+  // ! ==================================
+  const url = "api/po/hr/passport-request";
+  const apiRootUrl = process.env.API_ROOT_URL as string;
+  const { headers } = await getFetchHeaders();
+  const requestBody = {
+    employee_id: 1722,
+  };
+  const requestBodyString = JSON.stringify(requestBody);
+  const requestUrl = `${apiRootUrl}/${url}`;
+
+  // ! FETCH
+  // ! ==================================
+  const apiResponse = await fetch(requestUrl, {
+    headers,
+    method: "POST",
+    body: requestBodyString,
+  });
+  const responseJson = await apiResponse.json();
+
+  // ! VALIDATION
+  // ! ==================================
+  const validatedResponse = ResponseSchema.parse(responseJson);
+  const { result } = validatedResponse;
+  const { data } = result;
+  const validatedData = PassportRequestSchema.array().parse(data);
+
+  // ! PARSING
+  // ! ==================================
+
+  const getStringValue = (field: unknown): string =>
+    typeof field === "string" ? field : "";
+
+  const returnedData: PassportRequest[] = validatedData.map((data) => {
+    const newsItem: PassportRequest = {
+      id: getStringValue(data.name),
+      date: getStringValue(data.date),
+      passportNumber: getStringValue(data.new_passport),
+      passportExpireDate: getStringValue(data.passport_end_date),
+      status: getStringValue(data.state),
+    };
+    return newsItem;
+  });
+
+  return returnedData;
 };
 
-const PassportDummyData: PassportRequest[] = [
+const dummyData: PassportRequest[] = [
   {
     id: "#55465",
     date: "2024-05-05",
