f5b2e2f389ee7eb8ebbe4caf9cd9e2f86a7e14ec
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-01 17:30:10 +0300

    add individual goal modal component

--- app/lib/modalPaths.ts
+++ app/lib/modalPaths.ts
@@ -63,4 +63,12 @@ export const modalPaths = {
     title: "إضافة مؤشر الأداء",
     href: "/modal/IndicatorModal",
   },
+  IndividualGoalModal: {
+    title: 'إضافة هدف فردي جديد',
+    href: '/modal/IndividualGoalModal',
+  },
+  IndicatorModal: {
+    title: 'إضافة مؤشر الأداء',
+    href: '/modal/IndicatorModal',
+  },
 } as const;
