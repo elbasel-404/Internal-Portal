2a42b7c90728301fe8643f5f3189e75dc11e6d65
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-01 13:18:47 +0300

    dynamic employee id

--- app/server/auth/getEmployeeId.ts
+++ app/server/auth/getEmployeeId.ts
@@ -1,54 +1,36 @@
 "use server";
 
-interface GetEmployeeIdParams {
-    username: string,
-    password: string,
-    accessToken: string,
-}
+import { ProfileElementSchema, ResponseSchema } from "@api/schemas";
+import { getFetchHeaders } from "../getFetchHeaders";
 
-// ! ======================= ENV =============================
-const API_ROOT_URL = process.env.API_ROOT_URL;
-const API_KEY = process.env.API_KEY
-const API_KEY_HEADER_NAME = process.env.API_KEY_HEADER_NAME
-const SESSION_ID = process.env.SESSION_ID
+export const getEmployeeId = async () => {
+    // ! VARIABLES
+    // ! ==================================
+    const url = "api/po/read/profile";
+    const apiRootUrl = process.env.API_ROOT_URL as string;
+    const { headers } = await getFetchHeaders();
+    const requestBody = {};
+    const requestBodyString = JSON.stringify(requestBody);
+    const requestUrl = `${apiRootUrl}/${url}`;
 
-// ! ======================= STATIC =============================
-const QUERY_URL = "http://172.25.54.80:8069"
-const FETCH_URL = `${API_ROOT_URL}/api/call/res.users/api_login`
-const FETCH_DB = "pre_prod"
-const FETCH_DEVICE = "SM-N970F"
+    // ! FETCH
+    // ! ==================================
+    const apiResponse = await fetch(requestUrl, {
+        headers,
+        method: "POST",
+        body: requestBodyString,
+    });
+    const responseJson = await apiResponse.json();
 
-export const getEmployeeId = async ({ username, password, accessToken }: GetEmployeeIdParams) => {
-    const query = {
-        url: QUERY_URL,
-        db: FETCH_DB,
-        username,
-        password,
-        device_type: FETCH_DEVICE
-    };
+    // ! VALIDATION
+    // ! ==================================
+    const validatedResponse = ResponseSchema.parse(responseJson);
+    const { result } = validatedResponse;
+    const { data } = result;
+    const validatedData = ProfileElementSchema.parse(data[0]);
 
-    const queryString = encodeURIComponent(JSON.stringify(query));
-    const fullUrl = `${FETCH_URL}?kwargs=${queryString}`;
-
-    try {
-        const response = await fetch(fullUrl, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                [API_KEY_HEADER_NAME]: API_KEY,
-                'Cookie': `session_id=${SESSION_ID}`
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const employeeId = await response.text();
-        return employeeId;
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw error;
-    }
+    // ! PARSING
+    const employeeId = validatedData.id.toString();
+    return employeeId;
 };
 
--- app/server/getFetchHeaders.ts
+++ app/server/getFetchHeaders.ts
@@ -1,10 +1,20 @@
 "use server";
 
+import { getSession } from "@auth";
+
 export const getFetchHeaders = async () => {
   const API_KEY = process.env.API_KEY as string;
   const API_KEY_HEADER_NAME = process.env.API_KEY_HEADER_NAME as string;
-  const BEARER_TOKEN = process.env.BEARER_TOKEN as string;
   const SESSION_ID = process.env.SESSION_ID as string;
+
+  const session = await getSession();
+  if (!session) {
+    console.error("No session found");
+    console.log({ session })
+    throw new Error("No session found");
+  }
+  const { access_token } = session;
+  const BEARER_TOKEN = access_token;
 
   if (!API_KEY || !API_KEY_HEADER_NAME || !BEARER_TOKEN || !SESSION_ID) {
     console.log({
