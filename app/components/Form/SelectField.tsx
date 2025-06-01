a8b083510b226c67b2d5d64c8fb7e2ae5f781cf1
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-01 16:08:47 +0300

    pass className prop to each of these fields

--- app/components/Form/SelectField.tsx
+++ app/components/Form/SelectField.tsx
@@ -7,6 +7,7 @@ interface SelectProps {
   label: string;
   name: string;
   placeholder?: string;
+  className?: string;
   required?: boolean;
   value?: string;
   onChange?: (value: string) => void;
@@ -18,6 +19,7 @@ export const SelectField = ({
   name,
   placeholder = 'اختر النوع',
   required = false,
+  className,
   value,
   onChange,
 }: SelectProps) => {
@@ -25,7 +27,7 @@ export const SelectField = ({
     <div className='flex flex-col gap-1 w-full'>
       <label className='font-medium text-foreground'>
         {label}
-        {required && <span className="text-red-500">*</span>}
+        {required && <span className='text-red-500'>*</span>}
       </label>
       <Select
         name={name}
@@ -33,6 +35,7 @@ export const SelectField = ({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        className={className}
       />
     </div>
   );
