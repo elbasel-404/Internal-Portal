0d529c91cb34ba8070a27d90a8f6250babec6013
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-01 13:18:47 +0300

    dynamic employee id

--- app/server/auth/getEmployeeId.ts
+++ app/server/auth/getEmployeeId.ts
@@ -4,32 +4,32 @@ import { ProfileElementSchema, ResponseSchema } from "@api/schemas";
 import { getFetchHeaders } from "../getFetchHeaders";
 
 export const getEmployeeId = async () => {
-  // ! VARIABLES
-  // ! ==================================
-  const url = "api/po/read/profile";
-  const apiRootUrl = process.env.API_ROOT_URL as string;
-  const { headers } = await getFetchHeaders();
-  const requestBody = {};
-  const requestBodyString = JSON.stringify(requestBody);
-  const requestUrl = `${apiRootUrl}/${url}`;
+    // ! VARIABLES
+    // ! ==================================
+    const url = "api/po/read/profile";
+    const apiRootUrl = process.env.API_ROOT_URL as string;
+    const { headers } = await getFetchHeaders();
+    const requestBody = {};
+    const requestBodyString = JSON.stringify(requestBody);
+    const requestUrl = `${apiRootUrl}/${url}`;
 
-  // ! FETCH
-  // ! ==================================
-  const apiResponse = await fetch(requestUrl, {
-    headers,
-    method: "POST",
-    body: requestBodyString,
-  });
-  const responseJson = await apiResponse.json();
+    // ! FETCH
+    // ! ==================================
+    const apiResponse = await fetch(requestUrl, {
+        headers,
+        method: "POST",
+        body: requestBodyString,
+    });
+    const responseJson = await apiResponse.json();
 
-  // ! VALIDATION
-  // ! ==================================
-  const validatedResponse = ResponseSchema.parse(responseJson);
-  const { result } = validatedResponse;
-  const { data } = result;
-  const validatedData = ProfileElementSchema.parse(data[0]);
+    // ! VALIDATION
+    // ! ==================================
+    const validatedResponse = ResponseSchema.parse(responseJson);
+    const { result } = validatedResponse;
+    const { data } = result;
+    const validatedData = ProfileElementSchema.parse(data[0]);
 
-  // ! PARSING
-  const employeeId = validatedData.id.toString();
-  return employeeId;
+    // ! PARSING
+    const employeeId = validatedData.id.toString();
+    return employeeId;
 };
