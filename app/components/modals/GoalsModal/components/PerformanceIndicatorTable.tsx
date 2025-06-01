7606886ea954bd97c6a2ca6e41f0f4c8110e20a5
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-01 16:10:48 +0300

    add performance table component

--- app/components/modals/GoalsModal/components/PerformanceIndicatorTable.tsx
+++ app/components/modals/GoalsModal/components/PerformanceIndicatorTable.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { FolderWithSearchIcon } from '@icons';
-import { Indicator } from '@types';
+import { FolderWithSearchIcon } from "@icons";
+import { Indicator } from "@types";
 import {
   Table,
   TableBody,
@@ -9,18 +9,18 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@ui';
-import { useState } from 'react';
-import { AddNewGoalButton } from './AddNewGoalButton';
-import { IndicatorRow } from './IndicatorRow';
+} from "@ui";
+import { useState } from "react";
+import { AddNewGoalButton } from "./AddNewGoalButton";
+import { IndicatorRow } from "./IndicatorRow";
 
 const headers = [
-  { label: 'مؤشر الأداء' },
-  { label: 'وزن المؤشر' },
-  { label: 'خطوات الإنجاز' },
-  { label: 'مقياس المؤشر' },
-  { label: 'نوع المستهدف' },
-  { label: 'المستهدف (رقم/تاريخ)' },
+  { label: "مؤشر الأداء" },
+  { label: "وزن المؤشر" },
+  { label: "خطوات الإنجاز" },
+  { label: "مقياس المؤشر" },
+  { label: "نوع المستهدف" },
+  { label: "المستهدف (رقم/تاريخ)" },
 ];
 
 interface PerformanceIndicatorsTableProps {
@@ -41,18 +41,18 @@ const PerformanceIndicatorsTable = ({
   };
 
   return (
-    <div className='container mx-auto'>
-      <div className='flex flex-col md:flex-row gap-4 items-start md:items-center justify-between bg-primary-opacity py-4 px-[18px] border-r-[3px] border-r-primary'>
-        <p className='text-xl font-bold text-foreground'>مؤشر الأداء</p>
+    <div className="container mx-auto">
+      <div className="flex flex-col md:flex-row gap-4 items-start md:items-center justify-between bg-primary-opacity py-4 px-[18px] border-r-[3px] border-r-primary">
+        <p className="text-xl font-bold text-foreground">مؤشر الأداء</p>
       </div>
 
       <Table>
-        <TableHeader className='bg-cloudGray'>
+        <TableHeader className="bg-cloudGray">
           <TableRow>
             {headers.map((head, index) => (
               <TableHead
                 key={index}
-                className='text-right text-darkBlue text-sm px-4'
+                className="text-right text-darkBlue text-sm px-4"
               >
                 {head.label}
               </TableHead>
@@ -62,10 +62,10 @@ const PerformanceIndicatorsTable = ({
         <TableBody>
           {indicators.length === 0 ? (
             <TableRow>
-              <TableCell colSpan={6} className='text-center py-10'>
-                <div className='flex flex-col items-center justify-center gap-6'>
+              <TableCell colSpan={6} className="text-center py-10">
+                <div className="flex flex-col items-center justify-center gap-6">
                   <FolderWithSearchIcon />
-                  <p className='text-grey-400 text-xl font-medium'>
+                  <p className="text-grey-400 text-xl font-medium">
                     لم يعثر على أي سجلات
                   </p>
                 </div>
@@ -83,21 +83,21 @@ const PerformanceIndicatorsTable = ({
         </TableBody>
       </Table>
 
-      <div className='px-4 flex flex-col gap-4 mt-6'>
-        <AddNewGoalButton modalName='IndicatorModal'>
+      <div className="px-4 flex flex-col gap-4 mt-6">
+        <AddNewGoalButton modalName="IndicatorModal">
           إضافة مؤشر جديد
         </AddNewGoalButton>
-        <div className='w-full p-4 bg-[#00A65A] bg-opacity-15 rounded-lg mt-4'>
-          <h3 className='text-darkBlue font-medium mb-2'>
+        <div className="w-full p-4 bg-[#00A65A] bg-opacity-15 rounded-lg mt-4">
+          <h3 className="text-darkBlue font-medium mb-2">
             الحد الأدنى لعدد مؤشرات الأداء: 2، مع تحديد وزن كل مؤشر بين 5% و95%.
           </h3>
-          <div className='w-full bg-white h-3'>
+          <div className="w-full bg-white h-3">
             <div
-              className='bg-[#00A65A] h-3 transition-all duration-300'
+              className="bg-[#00A65A] h-3 transition-all duration-300"
               style={{ width: `100%` }}
             />
           </div>
-          <div className='font-medium text-foreground text-lg'>{100}%</div>
+          <div className="font-medium text-foreground text-lg">{100}%</div>
         </div>
       </div>
     </div>
