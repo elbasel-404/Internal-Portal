09d1d09a2f5155a333f4d19367b5d66643dc4cda
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-01 13:18:47 +0300

    dynamic employee id

--- app/server/getFetchHeaders.ts
+++ app/server/getFetchHeaders.ts
@@ -25,6 +25,15 @@ export const getFetchHeaders = async () => {
   const { access_token } = session;
   const BEARER_TOKEN = access_token;
 
+  const session = await getSession();
+  if (!session) {
+    console.error("No session found");
+    console.log({ session })
+    throw new Error("No session found");
+  }
+  const { access_token } = session;
+  const BEARER_TOKEN = access_token;
+
   if (!API_KEY || !API_KEY_HEADER_NAME || !BEARER_TOKEN || !SESSION_ID) {
     console.log({
       API_KEY,
