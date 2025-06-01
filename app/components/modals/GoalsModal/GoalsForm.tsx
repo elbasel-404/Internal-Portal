69f95e8416334662f98b37404fd9f859615ceee1
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-01 16:11:19 +0300

    update goal form component

--- app/components/modals/GoalsModal/GoalsForm.tsx
+++ app/components/modals/GoalsModal/GoalsForm.tsx
@@ -1,25 +1,16 @@
 'use client';
 
-import { Table } from '@components';
+import { Indicator } from '@types';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import {
-  AddNewGoalButton,
   FormActions,
   GoalDetailsSection,
   GoalsFormHeader,
   IndividualGoalSection,
   OrganizationalGoalsSection,
 } from './components';
-
-const tableHeaders = [
-  { label: 'مؤشر الأداء' },
-  { label: 'وزن المؤشر %' },
-  { label: 'خطوات الإنجاز' },
-  { label: 'مقياس المؤشر' },
-  { label: 'نوع المستهدف' },
-  { label: 'المستهدف (رقم/تاريخ)' },
-];
+import PerformanceIndicatorsTable from './components/PerformanceIndicatorTable';
 
 interface FormData {
   organizationalGoalLevel1: string;
@@ -32,7 +23,11 @@ interface FormData {
   endDate: Date;
 }
 
-export const GoalsForm = () => {
+interface GoalFormProps {
+  indicatorData: Indicator[];
+}
+
+export const GoalsForm = ({ indicatorData }: GoalFormProps) => {
   const router = useRouter();
 
   const [formData, setFormData] = useState<FormData>({
@@ -94,35 +89,7 @@ export const GoalsForm = () => {
         />
       </div>
 
-      <div>
-        <div className='flex flex-col md:flex-row gap-4 items-start md:items-center justify-between bg-primary-opacity py-4 px-[18px] border-r-[3px] border-r-primary'>
-          <p className='text-xl font-bold text-foreground'>مؤشر الأداء</p>
-        </div>
-        <Table
-          toggleId={false}
-          columns={tableHeaders}
-          rows={[]}
-          tableClassName='h-fit'
-        />
-        <div className='px-4 flex flex-col gap-4'>
-          <AddNewGoalButton modalName='GoalsModal'>
-            إضافة مؤشر جديد
-          </AddNewGoalButton>
-          <div className='w-full p-4 bg-[#00A65A] bg-opacity-15 rounded-lg mt-4'>
-            <h3 className='text-darkBlue font-medium mb-2'>
-              الحد الأدنى لعدد مؤشرات الأداء: 2، مع تحديد وزن كل مؤشر بين 5%
-              و95%.
-            </h3>
-            <div className='w-full bg-white h-3'>
-              <div
-                className='bg-[#00A65A] h-3 transition-all duration-300'
-                style={{ width: `100%` }}
-              />
-            </div>
-            <div className='font-medium text-foreground text-lg'>{100}%</div>
-          </div>
-        </div>
-      </div>
+      <PerformanceIndicatorsTable indicatorData={indicatorData} />
 
       <FormActions onClose={closeModal} onSubmit={handleSubmit} />
     </form>
