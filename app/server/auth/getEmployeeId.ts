7a4c3669a1ef8d26070dc0fec44b187c6ac04904
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-01 13:18:47 +0300

    dynamic employee id

--- app/server/auth/getEmployeeId.ts
+++ app/server/auth/getEmployeeId.ts
@@ -1,55 +1,35 @@
 "use server";
 
-interface GetEmployeeIdParams {
-    username: string,
-    password: string,
-    accessToken: string,
-}
+import { ProfileElementSchema, ResponseSchema } from "@api/schemas";
+import { getFetchHeaders } from "../getFetchHeaders";
 
-// ! ======================= ENV =============================
-const API_KEY = "85ced9c9-b64b-4d76-85a5-ae3b869b044d";
-const API_KEY_HEADER_NAME = "x-api-key";
-const SESSION_ID = "808eef83a1110af844371b501c5e2ee77109d786";
+export const getEmployeeId = async () => {
+  // ! VARIABLES
+  // ! ==================================
+  const url = "api/po/read/profile";
+  const apiRootUrl = process.env.API_ROOT_URL as string;
+  const { headers } = await getFetchHeaders();
+  const requestBody = {};
+  const requestBodyString = JSON.stringify(requestBody);
+  const requestUrl = `${apiRootUrl}/${url}`;
 
-// ! ======================= STATIC =============================
-const QUERY_URL = "http://10.60.55.21";
-const FETCH_URL = `https://apis.monshaat.gov.sa/ERP/TaskService/api/call/res.users/api_login`;
-const FETCH_DB = "smea";
-const FETCH_DEVICE = "sdk_gphone64_x86_64";
+  // ! FETCH
+  // ! ==================================
+  const apiResponse = await fetch(requestUrl, {
+    headers,
+    method: "POST",
+    body: requestBodyString,
+  });
+  const responseJson = await apiResponse.json();
 
-export const getEmployeeId = async ({ username, password, accessToken }: GetEmployeeIdParams) => {
-    const query = {
-        url: QUERY_URL,
-        db: FETCH_DB,
-        // username: "asaedi.uat",
-        // password: "@112",
-        username,
-        password,
-        device_type: FETCH_DEVICE
-    };
+  // ! VALIDATION
+  // ! ==================================
+  const validatedResponse = ResponseSchema.parse(responseJson);
+  const { result } = validatedResponse;
+  const { data } = result;
+  const validatedData = ProfileElementSchema.parse(data[0]);
 
-    const queryString = encodeURIComponent(JSON.stringify(query));
-    const fullUrl = `${FETCH_URL}?kwargs=${queryString}`;
-
-    try {
-        const response = await fetch(fullUrl, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                // 'Authorization': `Bearer fIQ8RX0K8GYKuzw3I83XkduGs2ksh7`,
-                [API_KEY_HEADER_NAME]: API_KEY,
-                'Cookie': `session_id=${SESSION_ID}`
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const employeeId = await response.text();
-        return employeeId;
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw error;
-    }
+  // ! PARSING
+  const employeeId = validatedData.id.toString();
+  return employeeId;
 };
