76673edaf4d5fc3e35a5bf565876e978ab0cbde1
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-01 17:30:36 +0300

    add indicator modal component

---
+++ app/components/modals/IndicatorModal/IndicatorForm.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { InputField } from '@components/form';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
+import { FormActions } from './components/FormActions';
+
+export const IndicatorForm = () => {
+  const router = useRouter();
+  const [indicatorName, setIndicatorName] = useState<string>('');
+
+  const handleIndicatorNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIndicatorName(e.target.value);
+  };
+
+  const closeModal = () => {
+    router.back();
+  };
+
+  const handleSubmit = () => {
+    closeModal();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4 p-4'>
+      <InputField
+        label='اسم مؤشر الأداء'
+        name='indicatorName'
+        value={indicatorName}
+        onChange={handleIndicatorNameChange}
+        required
+      />
+      <FormActions onClose={closeModal} onSubmit={handleSubmit} />
+    </form>
+  );
+};
