74fd781a970a1463f59ae1c645ac96a95ee3aa1b
Yasser <yassermohamed654321@gmail.com>
2025-06-02 11:28:08 +0300

    finished

---
+++ app/(pages)/request/overtime-assignment/components/helpers/requestBodySchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const requestBodySchema = z.object({
+  employee_id: z.string(),
+  year: z.string(),
+  month: z.string(),
+  day_from: z.string(),
+  day_to: z.string(),
+  nb_hours: z.string(),
+  description: z.string(),
+});
