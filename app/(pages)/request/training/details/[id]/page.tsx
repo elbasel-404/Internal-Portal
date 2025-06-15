f0236557d97f7fd7a6ec4e34f8284a5a14da88a5
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-15 16:46:00 +0300

    update return button

--- app/(pages)/request/training/details/[id]/page.tsx
+++ app/(pages)/request/training/details/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { RequestDetails, RequestStatus, Table } from "@components"
 import { CheckIcon, XMarkIcon } from "@icons"
+import { paths } from "@lib"
 import { getRequestStatus, getTrainingDetails } from "@server"
 import { RequestHeader } from "@types"
 import { Button } from "@ui"
+import { RotateCcw } from "lucide-react"
+import Link from "next/link"
 
 type Params = Promise<{ id: string }>
 interface TrainingDetailsPageProps {
@@ -110,10 +113,15 @@ const TrainingDetailsPage = async ({ params }: TrainingDetailsPageProps) => {
             مرفوض
           </Button>
 
-          <Button className="flex group gap-1 items-center shadow-none hover:bg-red-600 hover:text-white justify-end text-destructive-foreground bg-destructive-opacity rounded-xl px-4 py-2.5">
-            <XMarkIcon className="fill-destructive-foreground group-hover:fill-white" />
-            تراجع
-          </Button>
+          <Link href={paths.training.href}>
+            <Button
+              type="button"
+              className="flex items-center gap-1 bg-[#DEE5ED] text-stormGray shadow-none hover:bg-gray-300 rounded-xl p-4"
+            >
+              <RotateCcw />
+              تراجع
+            </Button>
+          </Link>
         </div>
       </div>
     </>
