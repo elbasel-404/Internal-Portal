994b4ac5e02c17ec2229dcc7cab83e696988d8be
Yasser <yassermohamed654321@gmail.com>
2025-06-16 10:39:43 +0300

    will completed later

--- app/server/getOvertimeConfirmDetails.ts
+++ app/server/getOvertimeConfirmDetails.ts
@@ -1,21 +1,65 @@
 "use server"
 
+import { getDemo } from "../db/actions/getDemo"
+import { getFetchHeaders } from "./getFetchHeaders"
+import { OvertimeAssignmentElementSchema, ResponseSchema } from "@api/schemas"
+
 import { OvertimeConfirmDetails } from "@types"
 
 export const getOvertimeConfirmDetails = async (
   id: string,
 ): Promise<OvertimeConfirmDetails | void> => {
-  try {
-    const details: OvertimeConfirmDetails = {
-      id,
-      applicant: "خالد إبراهيم",
-      management: "إدارة تقنية المعلومات",
-      overTimeDuration: "5.0",
-      assignmentNumber: "7080",
-    }
-    return details
-  } catch (error) {
-    console.error("Error in getOvertimeConfirmDetails:", error)
-    return
+  const isDemo = await getDemo()
+  if (isDemo) return dummyData
+
+  // ! VARIBLES
+  // ! ==================================
+  const url = "api/po/hr/overtime_assignment"
+  const apiRootUrl = process.env.API_ROOT_URL as string
+  const { headers } = await getFetchHeaders()
+  const requestBody = {
+    id: id,
   }
+  const requestBodyString = JSON.stringify(requestBody)
+  const requestUrl = `${apiRootUrl}/${url}`
+
+  // ! FETCH
+  // ! ==================================
+  const apiResponse = await fetch(requestUrl, {
+    headers,
+    method: "POST",
+    body: requestBodyString,
+  })
+  const responseJson = await apiResponse.json()
+
+  // ! VALIDATION
+  // ! ==================================
+  const validatedResponse = ResponseSchema.parse(responseJson)
+  const { result } = validatedResponse
+  const { data } = result
+  const validatedData = OvertimeAssignmentElementSchema.parse(data[0])
+
+  // ! PARSING
+  // ! ==================================
+  const getArrayValue = (field: unknown, index: number = 1): string =>
+    Array.isArray(field) ? (field[index]?.toString() ?? "") : ""
+
+  const getStringValue = (field: unknown): string =>
+    typeof field === "string" ? field : ""
+
+  const returnedData: OvertimeConfirmDetails = {
+    id: getStringValue(validatedData.name),
+    applicant: getArrayValue(validatedData.employee_id),
+    management: getStringValue(validatedData.date_from),
+    overTimeDuration: getStringValue(validatedData.date_to),
+    assignmentNumber: getStringValue(validatedData.description),
+  }
+  return returnedData
+}
+const dummyData: OvertimeConfirmDetails = {
+  id: "#55470",
+  applicant: "خالد إبراهيم",
+  management: "إدارة تقنية المعلومات",
+  overTimeDuration: "5.0",
+  assignmentNumber: "7080",
 }
