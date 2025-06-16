060218083d42937639422c68b7f4283ed5a216d1
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-16 17:14:49 +0300

    add bank account change dynamic employee id

--- app/(pages)/request/bank-account-change/components/BankAccountForm.tsx
+++ app/(pages)/request/bank-account-change/components/BankAccountForm.tsx
@@ -77,7 +77,7 @@ export const BankAccountForm = ({ bankDetails }: BankAccountFormProps) => {
         label="نموذج طلب تغيير الحساب البنكي"
         path={paths.bankAccountChange.href}
       />
-      <input
+      {/* <input
         type="text"
         name="employee_id"
         id="employee_id"
@@ -86,7 +86,7 @@ export const BankAccountForm = ({ bankDetails }: BankAccountFormProps) => {
         readOnly
         value="1711"
         className="hidden"
-      />
+      /> */}
       <div className="p-4 space-y-6">
         <InputField
           label="الحساب الحالي للموظف"
