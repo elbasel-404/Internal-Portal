4d505c57c6081e6e6cbe8652f58bb363aedb7c5e
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-17 17:36:20 +0300

    make all schemas z.any

--- api-schemas/request-create-workflow/schema.ts
+++ api-schemas/request-create-workflow/schema.ts
@@ -1,8 +1,14 @@
 import * as z from "zod"
 
 export const RequestCreateWorkflowElementSchema = z.object({
-  state: z.string(),
+  state: z.any(),
 })
 export type RequestCreateWorkflowElement = z.infer<
   typeof RequestCreateWorkflowElementSchema
 >
+// export const RequestCreateWorkflowElementSchema = z.object({
+//   state: z.string(),
+// })
+// export type RequestCreateWorkflowElement = z.infer<
+//   typeof RequestCreateWorkflowElementSchema
+// >
