b02e9ba97aa1d8d09cd693c835322fc00f6f1322
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-18 15:09:03 +0300

    call training fields in new training page and pass it to form

--- app/(pages)/request/training/new/page.tsx
+++ app/(pages)/request/training/new/page.tsx
@@ -1,6 +1,6 @@
 import { RequestStatus } from "@components"
 import { getUser } from "@db/actions"
-import { getRequestStatus, getUserId } from "@server"
+import { getRequestStatus, getTrainingFields, getUserId } from "@server"
 import { TrainingForm } from "../components"
 
 const NewTrainingPage = async () => {
@@ -8,13 +8,25 @@ const NewTrainingPage = async () => {
   if (!userId) return
 
   const { trainingCourses } = await getUser(userId)
+  const trainingCenterFields = await getTrainingFields("training_center_id")
+  const trainingTravelDaysSettingsFields = await getTrainingFields("travel_days_setting")
+  const trainingTypeFields = await getTrainingFields("training_type_id")
+  const trainingCountryFields = await getTrainingFields("country_id")
+  const trainingCityFields = await getTrainingFields("city_id")
   const requestStatus = await getRequestStatus()
   const requestCaption =
     "انت الان في مرحلة انشاء الطلب و بانتظار موافقة المدير المباشر"
   return (
     <div className="space-y-4 mb-16">
       <RequestStatus status={requestStatus} caption={requestCaption} />
-      <TrainingForm trainingCourses={trainingCourses} />
+      <TrainingForm
+        trainingCourses={trainingCourses}
+        trainingCenterFields={trainingCenterFields}
+        trainingTravelDaysSettingsFields={trainingTravelDaysSettingsFields}
+        trainingTypeFields={trainingTypeFields}
+        trainingCountryFields={trainingCountryFields}
+        trainingCityFields={trainingCityFields}
+      />
     </div>
   )
 }
