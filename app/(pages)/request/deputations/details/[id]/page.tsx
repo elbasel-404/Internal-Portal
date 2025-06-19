f3f791d5731db10b9f60d83b7b81e09295646441
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-20 01:39:40 +0300

    update deputation details page

--- app/(pages)/request/deputations/details/[id]/page.tsx
+++ app/(pages)/request/deputations/details/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { RequestDetails, RequestStatus } from "@components"
-import { getRequestStatus, getDeputationRequestDetails } from "@server"
+import { ModalLink } from "@components/modals/ModalLink"
+import { AnglesLeftIcon } from "@icons"
+import { getDeputationRequestDetails, getRequestStatus } from "@server"
 import { RequestHeader } from "@types"
 import { ReactNode } from "react"
-import { AnglesLeftIcon } from "@icons"
-import { ModalLink } from "@components/modals/ModalLink"
 
 type Params = Promise<{ id: string }>
 
@@ -47,11 +47,6 @@ const DeputationDetailsPage = async ({
     deputationPlaces,
   } = (await getDeputationRequestDetails(id)) || {}
 
-  const isInternal = deputation === "داخلي"
-  const displayReason = status === "مرفوض"
-  const displayTrainingRequestNumber = deputationType === "رحلة تدريب"
-  const displayKilometers = transportation === "برا"
-
   const requestHeaders: RequestHeader[] = [
     {
       label: "رقم الطلب",
@@ -85,22 +80,16 @@ const DeputationDetailsPage = async ({
       label: "المدة",
       value: duration,
     },
-    ...(displayTrainingRequestNumber
-      ? [
-          {
-            label: "رقم طلب التدريب" as RequestHeader["label"],
-            value: trainingRequestNumber,
-          },
-        ]
-      : []),
-    ...(displayKilometers
-      ? [
-          {
-            label: "عدد الكيلومترات" as RequestHeader["label"],
-            value: kilometers,
-          },
-        ]
-      : []),
+    {
+      label: "رقم طلب التدريب" as RequestHeader["label"],
+      value: trainingRequestNumber,
+    },
+
+    {
+      label: "عدد الكيلومترات" as RequestHeader["label"],
+      value: kilometers,
+    },
+
     {
       label: "المهمة",
       value: task,
@@ -122,24 +111,19 @@ const DeputationDetailsPage = async ({
       value: reserved,
       key: "reserved",
     },
-    ...(isInternal
-      ? [
-          {
-            label: "المدينة" as RequestHeader["label"],
-            value: city,
-          },
-        ]
-      : [
-          {
-            label: "إصدار تأشيرة" as RequestHeader["label"],
-            value: issueVisa,
-            key: "issueVisa",
-          },
-          {
-            label: "الموظف البديل" as RequestHeader["label"],
-            value: replacementEmployee,
-          },
-        ]),
+    {
+      label: "المدينة" as RequestHeader["label"],
+      value: city,
+    },
+    {
+      label: "إصدار تأشيرة" as RequestHeader["label"],
+      value: issueVisa,
+      key: "issueVisa",
+    },
+    {
+      label: "الموظف البديل" as RequestHeader["label"],
+      value: replacementEmployee,
+    },
     {
       label: "تاريخ السفر للانتداب",
       value: travelStartDate,
@@ -156,30 +140,22 @@ const DeputationDetailsPage = async ({
       label: "تاريخ التحويل",
       value: transferDate,
     },
-    ...(displayReason
-      ? [
-          {
-            label: "سبب الرفض" as RequestHeader["label"],
-            value: reason,
-          },
-        ]
-      : []),
+    {
+      label: "سبب الرفض" as RequestHeader["label"],
+      value: reason,
+    },
     {
       label: "ملاحظات" as RequestHeader["label"],
       value: notes,
     },
-    ...(isInternal
-      ? []
-      : [
-          {
-            label: "مكان الانتداب" as RequestHeader["label"],
-            value: deputationPlaces as ReactNode,
-            tableHeaders: [
-              { label: "الدولة", key: "name" },
-              { label: "المدينة", key: "city" },
-            ],
-          },
-        ]),
+    {
+      label: "مكان الانتداب" as RequestHeader["label"],
+      value: deputationPlaces as ReactNode,
+      tableHeaders: [
+        { label: "الدولة", key: "name" },
+        { label: "المدينة", key: "city" },
+      ],
+    },
     {
       label: "المرفقات" as RequestHeader["label"],
       value: attachments,
