de8c564d51f2883493e16f9d0a9df5c20de2921f
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-22 22:28:36 +0300

    build successful II

--- app/(pages)/request/employee-members/components/helpers/formAction.ts
+++ app/(pages)/request/employee-members/components/helpers/formAction.ts
@@ -1,102 +1,12 @@
 "use server"
 
-import { getStoredEmployeeId } from "@auth"
-import { z } from "zod"
-import { CreateErrorSchema } from "../../../../../../api-schemas/CreateErrorSchema"
-import { CreateSuccessSchema } from "../../../../../../api-schemas/CreateSuccessSchema"
-import { getFetchHeaders } from "../../../../../server/getFetchHeaders"
+import { createData } from "../../../../../lib/createData"
 import { requestBodySchema } from "./requestBodySchema"
-import { State } from "./State"
 
-export const formAction = async (formData: FormData): Promise<State> => {
-  // ! ==================================
-  // ! VARIABLES
-  // ! ==================================
+export const formAction = async (formData: FormData) => {
   const endpointUrl = "api/po/hr/employee/members/create"
-  const rootUrl = process.env.API_ROOT_URL
-  const fetchHeaders = await getFetchHeaders()
-  const detailsHeaders = fetchHeaders?.headers
-  if (!detailsHeaders) {
-    return {
-      success: false,
-      errors: ["Failed to get fetchHeaders"],
-      id: null,
-    }
-  }
 
-  const requestBody = Object.fromEntries(formData.entries())
-  const employeeId = await getStoredEmployeeId()
-  requestBody["employee_id"] = employeeId ?? ""
-  const fetchUrl = `${rootUrl}/${endpointUrl}`
+  const result = await createData(endpointUrl, requestBodySchema, formData)
 
-  const headers = new Headers()
-  headers.append("Authorization", detailsHeaders.Authorization)
-  headers.append("x-api-key", detailsHeaders["x-api-key"])
-
-  // ! ==================================
-  // ! VALIDATION
-  // ! ==================================
-  const validation = requestBodySchema.safeParse(requestBody)
-  const { success, data: validatedRequestBody, error } = validation
-  if (!success) {
-    const validationErrors = error.format()
-
-    const validationErrorsEntries = Object.entries(validationErrors)
-    validationErrorsEntries.forEach(
-      ([, /* key removed to fix unused var */ value]) => {
-        if (
-          typeof value === "object" &&
-          value !== null &&
-          "_errors" in value &&
-          Array.isArray((value as { _errors: unknown })._errors)
-        ) {
-          const errors = value._errors
-          // const firstError: string = (errors as string[])[0] - removed unused variable
-          return { success: false, errors, id: null }
-        }
-      },
-    )
-  }
-
-  // ! ==================================
-  // ! FETCH
-  // ! ==================================
-  const fetchFormData = new FormData()
-  type RequestBody = z.infer<typeof requestBodySchema>
-
-  const validatedRequestBodyEntries = Object.entries(
-    validatedRequestBody as RequestBody,
-  )
-
-  validatedRequestBodyEntries.forEach(([key, value]) => {
-    fetchFormData.append(key, value as string | File)
-  })
-
-  const response = await fetch(fetchUrl, {
-    method: "POST",
-    headers,
-    body: fetchFormData,
-  })
-
-  const responseJson = await response.json()
-  const responseObject = responseJson.at(0)
-
-  const isBadRequest = response.status === 400
-  if (isBadRequest) {
-    const validatedResponseObject = CreateErrorSchema.parse(responseObject)
-    const { error /* status removed to fix unused var */ } =
-      validatedResponseObject
-
-    return {
-      success: false,
-      errors: [error],
-      id: null,
-    }
-  }
-
-  const validatedResponseObject = CreateSuccessSchema.parse(responseObject)
-  const { data /* message and status removed to fix unused vars */ } =
-    validatedResponseObject
-
-  return { success: true, errors: null, id: data.id }
+  return result
 }
