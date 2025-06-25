3509e46d030c63eb1a4ccaa763c0c06bfe0432f2
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-25 17:15:21 +0300

    fix

--- app/db/validation/validateUser.ts
+++ app/db/validation/validateUser.ts
@@ -2,8 +2,6 @@
 
 import { userSchema } from "@db/schemas"
 import type { User } from "@db/types"
-// import { logout } from "@auth"
-// import { clearUser } from "@server"
 
 export const validateUser = async (user: User): Promise<User> => {
   const userValid = userSchema.parse(user)
