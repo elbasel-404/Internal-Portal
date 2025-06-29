d5314166de209b5c9420abff4589e8ad0fc369a4
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-29 18:28:37 +0300

    pass deputation type and substitute employees as props to deputation form

--- app/(pages)/request/deputations/new/page.tsx
+++ app/(pages)/request/deputations/new/page.tsx
@@ -1,16 +1,25 @@
 import { CreateRequestStatus } from "@components"
-import { getCreateRequestStatus } from "@server"
+import {
+  getCreateRequestStatus,
+  getDeputationTypes,
+  getSubstituteEmployees,
+} from "@server"
 import { DeputationForm } from "../components"
 
 const NewDeputationPage = async () => {
   // const requestStatus = await getRequestStatus("", "")
+  const deputationTypes = await getDeputationTypes()
+  const substituteEmployees = await getSubstituteEmployees()
   const requestStatus = await getCreateRequestStatus("hr.deputation")
   const requestCaption =
     "انت الان في مرحلة انشاء الطلب و بانتظار موافقة المدير المباشر"
   return (
     <div className="space-y-4 mb-16">
       <CreateRequestStatus status={requestStatus} caption={requestCaption} />
-      <DeputationForm />
+      <DeputationForm
+        substituteEmployees={substituteEmployees}
+        deputationType={deputationTypes}
+      />
     </div>
   )
 }
