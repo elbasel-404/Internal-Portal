d8235a209bd1b58925ef07c0f1a1db09a90c19e3
alyadelaly <aliadelhacker123@yahoo.com>
2025-06-30 14:36:01 +0300

    add remove deputation location server component

---
+++ app/server/removeDeputationLocation.tsx
@@ -0,0 +1,23 @@
+"use server"
+
+import { db } from "@db"
+import { getUserIndex } from "@db/actions"
+import { getUserId } from "@server"
+
+export const removeDeputationLocation = async (index: number) => {
+  try {
+    await db.read()
+    const userId = await getUserId()
+    if (!userId) return
+
+    const userIndex = await getUserIndex(userId)
+
+    const userData = db.data.users[userIndex]
+
+    userData.deputationLocation.splice(index, 1)
+
+    await db.write()
+  } catch {
+    // Error handling could be added here in the future
+  }
+}
