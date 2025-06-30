aa7100a702a8a90bd49aee2b5a230be1e338b448
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-06-30 14:08:11 +0300

    revalidation for news

--- app/(pages)/home/@news/page.tsx
+++ app/(pages)/home/@news/page.tsx
@@ -8,14 +8,22 @@ import {
 } from "@server"
 // import { getUser } from "@db/actions"
 
+export const revalidate = 8400 // revalidate every 24 hours
+
 const NewsSlot = async () => {
   // const userId = await getUserId()
   const ads = await getAdsNewsList({
-    limit: 1,
+    limit: 2,
     page: 1,
   })
-  const news = await getNewsListRequests()
-  const familyNews = await getFamilyNewsList()
+  const news = await getNewsListRequests({
+    limit: 4,
+    page: 1,
+  })
+  const familyNews = await getFamilyNewsList({
+    limit: 4,
+    page: 1,
+  })
   // let tabs = defaultNewsTabs
   // if (userId) {
   //   const user = await getUser(userId)
