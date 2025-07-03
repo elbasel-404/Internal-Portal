a2a7c880a73c4f9bbf538ada5609b8b9d791aae3
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-03 14:53:26 +0300

    add new icons

---
+++ app/icons/Users1.icon.tsx
@@ -0,0 +1,54 @@
+export const Users1Icon = () => {
+  return (
+    <svg
+      width="34"
+      height="40"
+      viewBox="0 0 34 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.9547 1.33984H15.0647L12.3047 4.09984V7.99984L15.0647 10.7598H18.9547L21.7147 7.99984V4.09984L18.9547 1.33984Z"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M29.8613 6.98975H26.7413L24.5312 9.18975V12.3097L26.7413 14.5197H29.8613L32.0613 12.3097V9.18975L29.8613 6.98975Z"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M7.27313 6.98975H4.15312L1.95312 9.18975V12.3097L4.15312 14.5197H7.27313L9.48312 12.3097V9.18975L7.27313 6.98975Z"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M19.865 39.93L19.855 29.57L22.645 29.58V17.34L19.345 14.52H14.675L11.375 17.34V29.57H14.125L14.135 39.93"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M19.8619 39.93L19.8519 29.57L22.6419 29.58V17.34L19.3419 14.52H14.6719"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M30.1866 39.9298L30.1766 31.4598H32.9966V21.0998L29.6966 18.2798H25.4766"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <path
+        d="M3.82 39.9298L3.84 31.4598H1L1.01 21.0998L4.31 18.2798H8.53"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+    </svg>
+  )
+}
