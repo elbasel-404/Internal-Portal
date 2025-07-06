38b2e939cfcf1460542c8f811d04426015928f1c
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-06 19:21:00 +0300

    update purchase schema

--- api-schemas/purchase/schema.ts
+++ api-schemas/purchase/schema.ts
@@ -3,9 +3,17 @@ import * as z from "zod"
 export const PurchaseSchema = z.object({
   id: z.any(),
   employee_id: z.any(),
+  date: z.any(),
   description: z.any(),
   purchase_initiative_id: z.any(),
   project_name: z.any(),
   project_number: z.any(),
+  request_title: z.any(),
+  type: z.any(),
+  note: z.any(),
+  award_amount: z.any(),
+  amount_total: z.any(),
+  state: z.any(),
+  attachment_ids: z.any(),
 })
 export type PurchaseType = z.infer<typeof PurchaseSchema>
