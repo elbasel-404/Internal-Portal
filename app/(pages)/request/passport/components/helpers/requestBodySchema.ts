f07b52139185f8631f1a293d8e0f0fc81d5976d4
alyadelaly <aliadelhacker123@yahoo.com>
2025-07-10 13:45:59 +0300

    fix passport creation error

--- app/(pages)/request/passport/components/helpers/requestBodySchema.ts
+++ app/(pages)/request/passport/components/helpers/requestBodySchema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod"
 
 export const requestBodySchema = z.object({
-  employee_id: z.string(),
   new_passport: z.string(),
   passport_end_date: z.string(),
   attachment_ids: z.instanceof(File).or(z.string()),
