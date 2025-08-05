f4a40bc6e35fe94b49c9eaf09ea04e5bb6d72d11
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-08-05 11:00:10 +0300

    refactor

--- app/api-test/components/Output.tsx
+++ app/api-test/components/Output.tsx
@@ -1,27 +1,17 @@
-import dynamic from "next/dynamic"
-const ReactJsonView = dynamic(() => import("@microlink/react-json-view"), {
-  ssr: false,
-})
+// import dynamic from "next/dynamic"
+// const ReactJsonView = dynamic(() => import("@microlink/react-json-view"), {
+//   ssr: false,
+// })
+import { githubDarkTheme } from "@uiw/react-json-view/githubDark"
+import JsonView from "@uiw/react-json-view"
 
 export const Output = ({ json = {} }) => {
   return (
-    <ReactJsonView
-      theme="shapeshifter"
-      src={json}
-      //   src={{
-      // string: "this is a test string",
-      // integer: 42,
-      // array: [1, 2, 3, "test", NaN],
-      // float: 3.14159,
-      // undefined: undefined,
-      // object: {
-      //   "first-child": true,
-      //   "second-child": false,
-      //   "last-child": null,
-      // },
-      // string_number: "1234",
-      // date: new Date(),
-      //   }}
+    <JsonView
+      collapsed={true}
+      className="!text-2xl"
+      value={json}
+      style={githubDarkTheme}
     />
   )
 }
