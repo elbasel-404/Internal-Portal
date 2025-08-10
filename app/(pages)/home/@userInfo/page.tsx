c116007501c411b1611abe07cc78e9dd74e7c4e3
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-08-10 16:07:22 +0300

    fix

--- app/(pages)/home/@userInfo/page.tsx
+++ app/(pages)/home/@userInfo/page.tsx
@@ -1,7 +1,11 @@
-import { getProfileInfo } from "@server"
+import { getProfileInfo, getSession } from "@server"
 import { UserInfo } from "../components"
 
 const UserInfoSlot = async () => {
+  const session = await getSession()
+  if (!session) {
+    return null
+  }
   const user = await getProfileInfo()
   return <UserInfo key="user-info" user={user} />
 }
