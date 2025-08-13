d357bae863fccae359f232f2bd3da45bf2b09024
Yasser <yassermohamed654321@gmail.com>
2025-08-13 14:36:53 +0300

    commented unused code for use when integrate them

--- app/(pages)/request/supplier-evaluation/components/EvaluationData.tsx
+++ app/(pages)/request/supplier-evaluation/components/EvaluationData.tsx
@@ -1,50 +1,50 @@
-import { InfoGrid } from "@components"
-import { GeneralInfo } from "@types"
-import { colors } from "@lib"
+// import { InfoGrid } from "@components"
+// import { GeneralInfo } from "@types"
+// import { colors } from "@lib"
 
-const data: GeneralInfo[] = [
-  {
-    title: "الطلبات تحت الإجراء",
-    count: 0,
-    icon: "CalenderSpecialIcon",
-    id: -42,
-    backgroundColor: colors.light.primaryOpacity,
-    key: "employeeRequests",
-    active: true,
-    index: 0,
-  },
-  {
-    title: "الطلبات المعتمدة",
-    count: 0,
-    icon: "StampIcon",
-    id: -43,
-    backgroundColor: colors.light.primaryOpacity,
-    key: "employeeRequests",
-    active: true,
-    index: 1,
-  },
-  {
-    title: "الطلبات المرفوضة",
-    count: 0,
-    icon: "ClipboardIcon",
-    id: -44,
-    backgroundColor: colors.light.primaryOpacity,
-    key: "employeeRequests",
-    active: true,
-    index: 2,
-  },
-  {
-    title: "كل الطلبات",
-    count: 1,
-    icon: "ListAlternativeIcon",
-    id: -45,
-    backgroundColor: colors.light.primaryOpacity,
-    key: "employeeRequests",
-    active: true,
-    index: 3,
-  },
-]
+// const data: GeneralInfo[] = [
+//   {
+//     title: "الطلبات تحت الإجراء",
+//     count: 0,
+//     icon: "CalenderSpecialIcon",
+//     id: -42,
+//     backgroundColor: colors.light.primaryOpacity,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 0,
+//   },
+//   {
+//     title: "الطلبات المعتمدة",
+//     count: 0,
+//     icon: "StampIcon",
+//     id: -43,
+//     backgroundColor: colors.light.primaryOpacity,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 1,
+//   },
+//   {
+//     title: "الطلبات المرفوضة",
+//     count: 0,
+//     icon: "ClipboardIcon",
+//     id: -44,
+//     backgroundColor: colors.light.primaryOpacity,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 2,
+//   },
+//   {
+//     title: "كل الطلبات",
+//     count: 1,
+//     icon: "ListAlternativeIcon",
+//     id: -45,
+//     backgroundColor: colors.light.primaryOpacity,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 3,
+//   },
+// ]
 
-export const EvaluationData = () => {
-  return <InfoGrid info={data} />
-}
+// export const EvaluationData = () => {
+//   return <InfoGrid info={data} />
+// }
--- app/(pages)/request/work-document/components/WorkDocumentData.tsx
+++ app/(pages)/request/work-document/components/WorkDocumentData.tsx
@@ -1,64 +1,64 @@
-import { InfoGrid } from "@components"
-import { colors } from "@lib"
-import type { GeneralInfo } from "@types"
+// import { InfoGrid } from "@components"
+// import { colors } from "@lib"
+// import type { GeneralInfo } from "@types"
 
-const data: GeneralInfo[] = [
-  {
-    title: "الطلبات الجديدة",
-    count: 4,
-    icon: "FileWithPenIcon",
-    backgroundColor: colors.light.primaryOpacity,
-    id: -230,
-    key: "employeeRequests",
-    active: true,
-    index: 0,
-  },
-  {
-    title: "الطلبات تحت الإجراء",
-    count: 15,
-    icon: "CalenderSpecialIcon",
-    backgroundColor: colors.light.primaryOpacity,
-    id: -231,
-    key: "employeeRequests",
-    active: true,
-    index: 1,
-  },
-  {
-    title: "الطلبات المعتمدة",
-    count: 9,
-    icon: "StampIcon",
-    backgroundColor: colors.light.primaryOpacity,
-    id: -232,
-    key: "employeeRequests",
-    active: true,
-    index: 2,
-  },
-  {
-    title: "الطلبات المرفوضة",
-    count: 4,
-    icon: "ClipboardIcon",
-    backgroundColor: colors.light.primaryOpacity,
-    id: -233,
-    key: "employeeRequests",
-    active: true,
-    index: 3,
-  },
-  {
-    title: "كل الطلبات",
-    count: 32,
-    icon: "ListAlternativeIcon",
-    backgroundColor: colors.light.primaryOpacity,
-    id: -234,
-    key: "employeeRequests",
-    active: true,
-    index: 4,
-  },
-]
+// const data: GeneralInfo[] = [
+//   {
+//     title: "الطلبات الجديدة",
+//     count: 4,
+//     icon: "FileWithPenIcon",
+//     backgroundColor: colors.light.primaryOpacity,
+//     id: -230,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 0,
+//   },
+//   {
+//     title: "الطلبات تحت الإجراء",
+//     count: 15,
+//     icon: "CalenderSpecialIcon",
+//     backgroundColor: colors.light.primaryOpacity,
+//     id: -231,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 1,
+//   },
+//   {
+//     title: "الطلبات المعتمدة",
+//     count: 9,
+//     icon: "StampIcon",
+//     backgroundColor: colors.light.primaryOpacity,
+//     id: -232,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 2,
+//   },
+//   {
+//     title: "الطلبات المرفوضة",
+//     count: 4,
+//     icon: "ClipboardIcon",
+//     backgroundColor: colors.light.primaryOpacity,
+//     id: -233,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 3,
+//   },
+//   {
+//     title: "كل الطلبات",
+//     count: 32,
+//     icon: "ListAlternativeIcon",
+//     backgroundColor: colors.light.primaryOpacity,
+//     id: -234,
+//     key: "employeeRequests",
+//     active: true,
+//     index: 4,
+//   },
+// ]
 
-export const WorkDocumentData = () => {
-  return (
-    <div className="py-6">
-      <InfoGrid info={data} className="lg:grid-cols-2" />
-    </div>
-  )
-}
+// export const WorkDocumentData = () => {
+//   return (
+//     <div className="py-6">
+//       <InfoGrid info={data} className="lg:grid-cols-2" />
+//     </div>
+//   )
+// }
