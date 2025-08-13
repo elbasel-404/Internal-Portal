25494f40870650574cde9c42a400827b4e728ed8
Yasser <yassermohamed654321@gmail.com>
2025-08-13 14:36:53 +0300

    commented unused code for use when integrate them

--- app/(pages)/request/attendance/components/AttendanceData.tsx
+++ app/(pages)/request/attendance/components/AttendanceData.tsx
@@ -1,10 +1,10 @@
-import { InfoGrid } from "@components"
-import type { GeneralInfo } from "@types"
+// import { InfoGrid } from "@components"
+// import type { GeneralInfo } from "@types"
 
-interface AttendanceDataProps {
-  data: GeneralInfo[]
-}
+// interface AttendanceDataProps {
+//   data: GeneralInfo[]
+// }
 
-export const AttendanceData = ({ data }: AttendanceDataProps) => {
-  return <InfoGrid info={data} className="lg:grid-cols-3" />
-}
+// export const AttendanceData = ({ data }: AttendanceDataProps) => {
+//   return <InfoGrid info={data} className="lg:grid-cols-3" />
+// }
