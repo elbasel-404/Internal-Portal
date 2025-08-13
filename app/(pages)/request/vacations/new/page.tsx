0eafb584430f5b976a6334a8d772524e2fce0a01
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-13 16:48:23 +0300

    get substitute employees from endpoint

--- app/(pages)/request/vacations/new/page.tsx
+++ app/(pages)/request/vacations/new/page.tsx
@@ -1,16 +1,17 @@
 import { CreateRequestStatus } from "@components"
-import { getCreateRequestStatus, getVacationElements } from "@server"
+import { getCreateRequestStatus, getSubstituteEmployees, getVacationElements } from "@server"
 import { VacationForm } from "../components"
 
 const NewVacationPage = async () => {
   const model = "hr.holidays"
   const requestStatus = await getCreateRequestStatus(model)
   const vacationElements = await getVacationElements()
+  const substituteEmployees = await getSubstituteEmployees()
   const requestCaption = "انت الان في مرحلة انشاء الطلب"
   return (
     <div className="space-y-4 mb-16">
       <CreateRequestStatus status={requestStatus} caption={requestCaption} />
-      <VacationForm vacationElements={vacationElements} />
+      <VacationForm vacationElements={vacationElements} substituteEmployees={substituteEmployees} />
     </div>
   )
 }
