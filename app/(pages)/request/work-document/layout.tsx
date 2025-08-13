67aebec7d7276342947508000dbcdfd21d9c6312
Yasser <yassermohamed654321@gmail.com>
2025-08-13 11:29:59 +0300

    add comment for no integrated

--- app/(pages)/request/work-document/layout.tsx
+++ app/(pages)/request/work-document/layout.tsx
@@ -11,6 +11,7 @@ interface WorkDocumentLayoutProps {
 const WorkDocumentLayout = ({ children }: WorkDocumentLayoutProps) => {
   return (
     <>
+      {/* @TODO not integrated */}
       <RequestGeneralData model="holidays" />
       <section>{children}</section>
     </>
