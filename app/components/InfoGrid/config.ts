e6437a57c52fdb56cf48061705feaf26faf6bc1f
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-17 12:26:49 +0300

    fix medical ui bugs

--- app/components/InfoGrid/config.ts
+++ app/components/InfoGrid/config.ts
@@ -19,6 +19,7 @@ import {
   TabletAltIcon,
   TabletIcon,
   TagWithXIcon,
+  TimeBlueIcon,
   TimeIcon,
   TimeWithScreenIcon,
   TimerStartIcon,
@@ -26,7 +27,6 @@ import {
   WorkTimeIcon,
   WorkingHomeIcon,
   WritingNoteIcon,
-  TimeBlueIcon,
 } from "@icons"
 
 export const infoIcons = {
@@ -80,7 +80,7 @@ export const infoIcons = {
   done_employee_members_requests: StampIcon,
   refused_employee_members_requests: ClipboardIcon,
   all_employee_members_requests: ListAlternativeIcon,
-  pending_medical_insurance_requests: SandClockIcon,
+  pending_medical_insurance_requests: CalenderSpecialIcon,
   done_medical_insurance_requests: StampIcon,
   refuse_cancel_medical_insurance_requests: ClipboardIcon,
   all_medical_insurance_requests: ListAlternativeIcon,
