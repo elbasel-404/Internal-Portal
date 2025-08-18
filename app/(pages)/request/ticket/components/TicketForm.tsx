62d257f96a8736f9dbe2c591d562d238f52b5d44
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-08-18 17:47:34 +0300

    add routing

--- app/(pages)/request/ticket/components/TicketForm.tsx
+++ app/(pages)/request/ticket/components/TicketForm.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-import { createFileHandler } from "@atoms"
+// import { createFileHandler } from "@atoms"
 import {
-  AttachmentsField,
+  // AttachmentsField,
   FormHeader,
   RadioField,
   SubmitButton,
   TextareaField,
 } from "@components/form"
 import { paths } from "@lib"
-import { FileWithId } from "@types"
-import { useState } from "react"
+// import { FileWithId } from "@types"
+// import { useState } from "react"
 import {
   NonTechnicalRequest,
   TechnicalReport,
@@ -18,27 +18,27 @@ import {
 } from "./options"
 
 export const TicketForm = () => {
-  const [ticketType, setTicketType] = useState("technical")
-  const [techTicketType, setTechTicketType] = useState("reportProblem")
-  const [nonTechTicketType, setNonTechTicketType] = useState("request")
-  const [files, setFiles] = useState<FileWithId[]>([])
+  // const [ticketType, setTicketType] = useState("technical")
+  // const [techTicketType, setTechTicketType] = useState("reportProblem")
+  // const [nonTechTicketType, setNonTechTicketType] = useState("request")
+  // const [files, setFiles] = useState<FileWithId[]>([])
 
-  const handleTicketTypeChangeValue = (value: string) => {
-    setTicketType(value)
-  }
+  // const handleTicketTypeChangeValue = (value: string) => {
+  //   setTicketType(value)
+  // }
 
-  const handleTechTicketTypeChangeValue = (value: string) => {
-    setTechTicketType(value)
-  }
+  // const handleTechTicketTypeChangeValue = (value: string) => {
+  //   setTechTicketType(value)
+  // }
 
-  const handleNonTechTicketTypeChangeValue = (value: string) => {
-    setNonTechTicketType(value)
-  }
+  // const handleNonTechTicketTypeChangeValue = (value: string) => {
+  //   setNonTechTicketType(value)
+  // }
 
-  const fileHandler = createFileHandler(
-    () => files,
-    (newFiles) => setFiles(newFiles),
-  )
+  // const fileHandler = createFileHandler(
+  //   () => files,
+  //   (newFiles) => setFiles(newFiles),
+  // )
 
   return (
     <form className="bg-white rounded-md">
@@ -55,68 +55,69 @@ export const TicketForm = () => {
           labelStyle="font-medium text-base text-grey-500"
           radioStyle="flex-col md:flex-row"
           className="flex-col"
-          onChange={handleTicketTypeChangeValue}
-          selectedValue={ticketType}
+          // onChange={handleTicketTypeChangeValue}
+          // selectedValue={ticketType}
         />
-        {ticketType === "technical" && (
-          <RadioField
-            label="الرجاء اختيار نوع تذكرة التقنية"
-            name="technicalTicketType"
-            options={[
-              { value: "reportProblem", label: "الابلاغ عن مشكلة" },
-              { value: "request", label: "طلب خدمة" },
-            ]}
-            required={true}
-            labelStyle="font-medium text-base text-grey-500"
-            radioStyle="flex-col md:flex-row"
-            className="flex-col"
-            onChange={handleTechTicketTypeChangeValue}
-            selectedValue={techTicketType}
-          />
-        )}
-        {ticketType === "nonTechnical" && (
-          <RadioField
-            label="الرجاء اختيار نوع تذكرة الغير تقنية"
-            name="nonTechnicalTicketType"
-            options={[{ value: "request", label: "طلب خدمة" }]}
-            required={true}
-            labelStyle="font-medium text-base text-grey-500"
-            radioStyle="flex-col md:flex-row"
-            className="flex-col"
-            onChange={handleNonTechTicketTypeChangeValue}
-            selectedValue={nonTechTicketType}
-          />
-        )}
-        {ticketType === "nonTechnical" && nonTechTicketType === "request" && (
-          <NonTechnicalRequest />
-        )}
-        {ticketType === "technical" && techTicketType === "request" && (
-          <TechnicalRequest />
-        )}
-        {ticketType === "technical" && techTicketType === "reportProblem" && (
-          <TechnicalReport />
-        )}
+        {/* {ticketType === "technical" && ( */}
+        <RadioField
+          label="الرجاء اختيار نوع تذكرة التقنية"
+          name="technicalTicketType"
+          options={[
+            { value: "reportProblem", label: "الابلاغ عن مشكلة" },
+            { value: "request", label: "طلب خدمة" },
+          ]}
+          required={true}
+          labelStyle="font-medium text-base text-grey-500"
+          radioStyle="flex-col md:flex-row"
+          className="flex-col"
+          // onChange={handleTechTicketTypeChangeValue}
+          // selectedValue={techTicketType}
+        />
+        {/* )} */}
+        {/* {ticketType === "nonTechnical" && ( */}
+        <RadioField
+          label="الرجاء اختيار نوع تذكرة الغير تقنية"
+          name="nonTechnicalTicketType"
+          options={[{ value: "request", label: "طلب خدمة" }]}
+          required={true}
+          labelStyle="font-medium text-base text-grey-500"
+          radioStyle="flex-col md:flex-row"
+          className="flex-col"
+          // onChange={handleNonTechTicketTypeChangeValue}
+          // selectedValue={nonTechTicketType}
+        />
+        {/* )} */}
+        {/* {ticketType === "nonTechnical" && nonTechTicketType === "request" && ( */}
+        <NonTechnicalRequest />
+        {/* )} */}
+        {/* {ticketType === "technical" && techTicketType === "request" && ( */}
+        <TechnicalRequest />
+        {/* )} */}
+        {/* {ticketType === "technical" && techTicketType === "reportProblem" && ( */}
+        <TechnicalReport />
+        {/* )} */}
         <TextareaField
+          label="label"
           name="description"
-          label={
-            ticketType === "technical" && techTicketType === "reportProblem"
-              ? "وصف الإبلاغ"
-              : "وصف الطلب"
-          }
-          placeholder={
-            ticketType === "technical" && techTicketType === "reportProblem"
-              ? "اكتب عن وصف الإبلاغ"
-              : "اكتب عن وصف الطلب"
-          }
+          // label={
+          //   ticketType === "technical" && techTicketType === "reportProblem"
+          //     ? "وصف الإبلاغ"
+          //     : "وصف الطلب"
+          // }
+          // placeholder={
+          //   ticketType === "technical" && techTicketType === "reportProblem"
+          //     ? "اكتب عن وصف الإبلاغ"
+          //     : "اكتب عن وصف الطلب"
+          // }
           required
         />
-        <AttachmentsField
+        {/* <AttachmentsField
           handleFileUpload={fileHandler.upload}
           handleRemoveFile={(index: number) =>
             fileHandler.remove(files[index].id)
           }
           files={files}
-        />
+        /> */}
         <SubmitButton />
       </div>
     </form>
---
+++ app/(pages)/request/ticket/new/components/ButtonLink.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import type { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface ButtonLinkProps {
+  href: string
+  children: ReactNode
+  activeUrls: string[]
+}
+
+export const ButtonLink = ({ href, children, activeUrls }: ButtonLinkProps) => {
+  const pathName = usePathname()
+  const active = activeUrls.includes(pathName)
+  console.table({ pathName, url1: activeUrls[0], active })
+
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        "flex items-center gap-2 hover:ring focus:ring bg-[#007497]/10 ring-black transition-all cursor-pointer drop-shadow-lg w-fit p-4 rounded-lg",
+        active && "bg-[#007497]/20 text-[#007C9E] ",
+      )}
+    >
+      <span
+        className={twMerge(
+          "rounded-full flex items-center justify-center border-2 border-[#7996B9] w-5 h-5",
+          active && "border-[#007C9E]",
+        )}
+      >
+        {active && (
+          <span className="block w-2.5 h-2.5 rounded-full bg-[#007C9E]" />
+        )}
+      </span>
+      <span>{children}</span>
+    </Link>
+  )
+}
