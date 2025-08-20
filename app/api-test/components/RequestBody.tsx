f72eee280f9bf231190340c6774e4d5bea949325
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-08-20 11:12:50 +0300

    working state

--- app/api-test/components/RequestBody.tsx
+++ app/api-test/components/RequestBody.tsx
@@ -5,16 +5,9 @@ import { InputData } from "../types/InputData"
 import { Collapse } from "../ui/Collapse"
 import { useAtomValue } from "jotai"
 import { requestBodyAtom } from "@api/atoms/requestBodyAtom"
-import { useEffect } from "react"
 
 export const RequestBody = ({ body: body }: { body: InputData[] }) => {
   const requestBody = useAtomValue(requestBodyAtom)
-
-  useEffect(() => {}, [requestBody])
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <Collapse title="Request Body" className="flex-1">
