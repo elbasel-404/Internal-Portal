ea292c5c0e5a4a38cdc19900d211136ef37880e0
Yasser <yassermohamed654321@gmail.com>
2025-08-21 15:45:27 +0300

    not finished yet

---
+++ api-schemas/Contractor-list/schema.ts
@@ -0,0 +1,22 @@
+import * as z from "zod"
+
+export const ContractorListElementSchema = z.object({
+  id: z.any(),
+  name: z.any(),
+  custody_id_number: z.any(),
+  close_date: z.any(),
+  close_type: z.any(),
+  state: z.any(),
+  employee_id: z.any(),
+  order_date: z.any(),
+  custody_type: z.any(),
+  custody_amount: z.any(),
+  close_amount: z.any(),
+  remaining_amount: z.any(),
+  sector_id: z.any(),
+  job_id: z.any(),
+  administration_id: z.any(),
+  custody_reason: z.any(),
+  details: z.any(),
+})
+export type ContractorListElement = z.infer<typeof ContractorListElementSchema>
