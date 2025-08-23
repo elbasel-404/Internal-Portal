4a33e9c46c781e10af3f970f08e195e5dd132d29
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-23 17:35:10 +0300

    add types

---
+++ app/types/pages/request/ProductsType.ts
@@ -0,0 +1,10 @@
+export type ProductsType = {
+  id: string
+  classification: string
+  itemName: string
+  description: string
+  quantity: string
+  unitPrice: string
+  tax: string
+  totalAfterTax: string
+}
