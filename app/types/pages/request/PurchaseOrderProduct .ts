49931b9770e254df2bb06bd1559b03eaf672cdd5
alyadelaly <aliadelhacker123@yahoo.com>
2025-08-24 13:06:11 +0300

    add Purchase Order Product types

---
+++ app/types/pages/request/PurchaseOrderProduct .ts
@@ -0,0 +1,10 @@
+export interface PurchaseOrderProduct {
+  id: number
+  category: string
+  name: string
+  description: string
+  quantity: number
+  tax: string
+  unitPrice: number
+  completedQuantity: number
+}
