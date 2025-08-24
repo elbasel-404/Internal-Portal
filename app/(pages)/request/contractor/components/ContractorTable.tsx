1dca4cf27e7c60475af53bc8279530e8737b1349
Yasser <yassermohamed654321@gmail.com>
2025-08-24 12:55:41 +0300

    not finished yet

--- app/(pages)/request/contractor/components/ContractorTable.tsx
+++ app/(pages)/request/contractor/components/ContractorTable.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { FilterSection, Table } from "@components"
+// import { FilterSection, Table } from "@components"
+import { Table } from "@components"
 import { CirclePlusIcon, FilterIcon, SearchIcon } from "@icons"
 import { Button, Input } from "@ui"
 import Link from "next/link"
@@ -23,11 +24,11 @@ const tableHeaders = [
 export const ContractorTable = ({ data }: ContractorProps) => {
   const [, setCurrentPage] = useState(1)
   const [searchTerm, setSearchTerm] = useState("")
-  const [isFilterVisible, setIsFilterVisible] = useState(false)
+  // const [isFilterVisible, setIsFilterVisible] = useState(false)
 
-  const toggleFilter = () => {
-    setIsFilterVisible((prev) => !prev)
-  }
+  // const toggleFilter = () => {
+  //   setIsFilterVisible((prev) => !prev)
+  // }
 
   if (!data) return
 
@@ -56,7 +57,7 @@ export const ContractorTable = ({ data }: ContractorProps) => {
               onChange={handleSearch}
             />
             <Button
-              onClick={toggleFilter}
+              // onClick={toggleFilter}
               className="bg-cloudGray p-2.5 rounded-full shadow-none hover:bg-cloudGray"
             >
               <FilterIcon className="fill-primary" />
@@ -71,9 +72,9 @@ export const ContractorTable = ({ data }: ContractorProps) => {
           </div>
         </div>
 
-        {isFilterVisible && (
+        {/* {isFilterVisible && (
           <FilterSection options={data} onApplyFilters={() => {}} />
-        )}
+        )} */}
 
         <Table
           columns={tableHeaders}
