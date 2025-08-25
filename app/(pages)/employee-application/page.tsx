c8822f3ab655e1954cf47b2dfede708420d006ca
Yasser <yassermohamed654321@gmail.com>
2025-08-25 15:04:31 +0300

    change images

--- app/(pages)/employee-application/page.tsx
+++ app/(pages)/employee-application/page.tsx
@@ -10,7 +10,7 @@ const EmployeeApplicationPage = () => {
     },
     {
       image: "app-2.svg",
-      buttonText: "لمتجر Apple غير سعودي",
+      buttonText: "بصيغة APK",
       url: "/",
     },
     {
